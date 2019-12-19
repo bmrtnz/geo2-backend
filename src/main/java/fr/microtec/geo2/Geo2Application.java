ecb51ffeead46a00a070872f1140b185af7972b1
valentin <valentin@microtec.fr>
2019-12-19 15:58:02 +0100

    Add spring boot

---
+++ src/main/java/fr/microtec/geo2/Geo2Application.java
@@ -0,0 +1,13 @@
+package fr.microtec.geo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Geo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Geo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/fr/microtec/geo2/Geo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.microtec.geo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Geo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
