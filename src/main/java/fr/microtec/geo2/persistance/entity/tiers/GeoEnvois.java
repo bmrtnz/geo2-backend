26c11ffdba2adbbfe521fc3c9b7246bc603c06d4
Thomas ROLLIN <thomas@microtec.fr>
2021-06-21 10:24:55 +0200

    update GeoEnvois entity#11010

--- src/main/java/fr/microtec/geo2/persistance/entity/tiers/GeoEnvois.java
+++ src/main/java/fr/microtec/geo2/persistance/entity/tiers/GeoEnvois.java
@@ -1,5 +1,7 @@
 package fr.microtec.geo2.persistance.entity.tiers;
 
+import java.time.LocalDate;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -27,8 +29,56 @@ public class GeoEnvois extends ModifiedEntity {
 	@JoinColumn(name = "ord_ref")
 	private GeoOrdre ordre;
 
+	@Column(name = "nordre")
+	private String numeroOrdre;
+
+	@Column(name = "version_ordre")
+	private String versionOrdre;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "per_codecom")
+	private GeoPersonne assistante;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "per_codeass")
+	private GeoPersonne commercial;
+
   @ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "flu_code")
 	private GeoFlux flux;
+
+	@Column(name = "tyt_code")
+  private Character typeTiers;
+
+	@Column(name = "tie_code")
+  private String codeTiers;
+
+	@Column(name = "acces1")
+  private String numeroAcces1;
+
+	@Column(name = "demdat")
+  private LocalDate dateDemande;
+
+	@Column(name = "envdat")
+  private LocalDate dateEnvoi;
+
+	@Column(name = "ackdat")
+  private LocalDate dateAccuseReception;
+
+	@Column(name = "nbtent")
+  private Integer nombreTentatives;
+
+	@Column(name = "imp_id")
+  private String imprimanteID;
+
+	@Column(name = "env_desc")
+  private String commentairesAvancement;
+
+	@Column(name = "doc_filename")
+  private String nomFichier;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "moc_code")
+	private GeoMoyenCommunication moyenCommunication;
 
 }
