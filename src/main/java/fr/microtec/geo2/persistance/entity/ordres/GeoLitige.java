ff1927e676663f43d69f810d92a0ac7cfd306c18
Thomas ROLLIN <thomas@microtec.fr>
2021-06-22 17:43:20 +0200

    update litige entity #11010

--- src/main/java/fr/microtec/geo2/persistance/entity/ordres/GeoLitige.java
+++ src/main/java/fr/microtec/geo2/persistance/entity/ordres/GeoLitige.java
@@ -1,5 +1,6 @@
 package fr.microtec.geo2.persistance.entity.ordres;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -10,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import fr.microtec.geo2.persistance.entity.ValidateAndModifiedEntity;
 import lombok.Data;
@@ -25,14 +27,85 @@ public class GeoLitige extends ValidateAndModifiedEntity {
 	@Column(name = "lit_ref")
 	private String id;
 
+	@NotNull
   @ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "ord_ref_origine")
+	@JoinColumn(name = "ord_ref_origine",nullable = false)
 	private GeoOrdre ordreOrigine;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "ord_ref_avoir")
+	private GeoOrdre ordreAvoirClient;
+
+	@Column(name = "ord_ref_avoir_fourni")
+	private String ordreAvoirFournisseurReference;
+
+	@Column(name = "ord_ref_replace")
+	private String ordreReplacementReference;
 
 	@OneToMany(fetch = FetchType.LAZY, mappedBy = "litige")
 	private List<GeoLitigeLigne> lignes;
 
   @Column(name = "lit_frais_annexes")
 	private Float fraisAnnexes;
+
+  @Column(name = "lit_frais_comment")
+	private Float fraisAnnexesCommentaires;
+
+  @Column(name = "lca_code")
+	private String causeLitigeCode;
+
+  @Column(name = "lcq_code")
+	private String consequenceLitigeCode;
+
+  @Column(name = "tyt_code")
+	private Character responsableTiers;
+
+  @Column(name = "tie_code")
+	private String responsableTiersCode;
+
+  @Column(name = "ref_cli")
+	private String referenceClient;
+
+  @Column(name = "lit_comment")
+	private String commentairesInternes;
+
+  @Column(name = "res_comment")
+	private String commentairesResponsable;
+
+  @Column(name = "lit_date_creation")
+	private LocalDate dateCreation;
+
+  @Column(name = "lit_date_origine")
+	private LocalDate dateOrigine;
+
+  @Column(name = "lit_date_resolution")
+	private LocalDate dateResolution;
+
+  @Column(name = "lit_date_avoir_client")
+	private LocalDate dateAvoirClient;
+
+  @Column(name = "lit_date_avoir_fourni")
+	private LocalDate dateAvoirFournisseur;
+
+  @Column(name = "date_doc")
+	private LocalDate dateEnvoiDocuments;
+
+  @Column(name = "fl_encours")
+	private Boolean enCoursNegociation;
+
+  @Column(name = "fl_client_clos")
+	private Boolean clientCloture;
+
+  @Column(name = "fl_fourni_clos")
+	private Boolean fournisseurCloture;
+
+  @Column(name = "fl_client_admin")
+	private Boolean clientValideAdmin;
+
+  @Column(name = "fl_fourni_admin")
+	private Boolean fournisseurValideAdmin;
+
+  @Column(name = "num_version")
+	private Integer numeroVersion;
 
 }
