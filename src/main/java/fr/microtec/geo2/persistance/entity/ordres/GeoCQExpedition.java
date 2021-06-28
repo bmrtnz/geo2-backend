266408b7168c4c29a36d29797642dc374ab21718
Thomas ROLLIN <thomas@microtec.fr>
2021-06-28 14:21:40 +0200

    format CQExpedition #11010

--- src/main/java/fr/microtec/geo2/persistance/entity/ordres/GeoCQExpedition.java
+++ src/main/java/fr/microtec/geo2/persistance/entity/ordres/GeoCQExpedition.java
@@ -4,8 +4,6 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Convert;
-import javax.persistence.Embeddable;
-import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Id;
