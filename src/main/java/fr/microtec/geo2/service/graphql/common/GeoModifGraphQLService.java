6e43561a3019e5c252f95da8a41bfc5ac8527787
Thomas ROLLIN <thomas@microtec.fr>
2021-10-19 17:53:12 +0200

    add geo_modif GraphQL endpoint #14313

---
+++ src/main/java/fr/microtec/geo2/service/graphql/common/GeoModifGraphQLService.java
@@ -0,0 +1,51 @@
+package fr.microtec.geo2.service.graphql.common;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Service;
+
+import fr.microtec.geo2.configuration.graphql.RelayPage;
+import fr.microtec.geo2.persistance.entity.common.GeoModif;
+import fr.microtec.geo2.persistance.repository.common.GeoModifRepository;
+import fr.microtec.geo2.service.graphql.GeoAbstractGraphQLService;
+import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLEnvironment;
+import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLNonNull;
+import io.leangen.graphql.annotations.GraphQLQuery;
+import io.leangen.graphql.execution.ResolutionEnvironment;
+import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
+
+@Service
+@GraphQLApi
+@Secured("ROLE_USER")
+public class GeoModifGraphQLService extends GeoAbstractGraphQLService<GeoModif, Integer> {
+
+	public GeoModifGraphQLService(GeoModifRepository repository) {
+		super(repository, GeoModif.class);
+	}
+
+	@GraphQLQuery
+	public RelayPage<GeoModif> allModif(
+			@GraphQLArgument(name = "search") String search,
+			@GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
+			@GraphQLEnvironment ResolutionEnvironment env
+	) {
+		return this.getPage(search, pageable, env);
+	}
+
+	@GraphQLQuery
+	public Optional<GeoModif> getModif(
+			@GraphQLArgument(name = "id") Integer id
+	) {
+		return super.getOne(id);
+  }
+
+  	@GraphQLMutation
+	public GeoModif saveModif(GeoModif Modif) {
+		return this.save(Modif);
+	}
+
+}
