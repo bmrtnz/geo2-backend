518bc9fc9867fe3dfceeedd9250aef0a9080b3a4
Thomas ROLLIN <thomas@microtec.fr>
2021-12-20 14:26:24 +0100

    implement `CodePalox` API #14555

---
+++ src/main/java/fr/microtec/geo2/persistance/entity/tiers/GeoSupervisionPalox.java
@@ -0,0 +1,61 @@
+package fr.microtec.geo2.persistance.entity.tiers;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+public abstract class GeoSupervisionPalox {
+
+  @Id
+	@Column(name = "rownum")
+  Integer id;
+
+  @Column(name = "cli_code")
+	String codeClient;
+
+  @Column(name = "cen_code")
+	String codeEntrepot;
+
+  @Column(name = "e_raisoc")
+	String raisonSocialeEntrepot;
+
+  @Column(name = "zip")
+	String codePostalEntrepot;
+
+  @Column(name = "ville")
+	String villeEntrepot;
+
+  @Column(name = "pay_code")
+	String codePaysEntrepot;
+
+  @Column(name = "col_code")
+	String codeEmballage;
+
+  @Column(name = "fou_code")
+	String codeFournisseur;
+
+  @Column(name = "raisoc")
+	String raisonSocialeFournisseur;
+
+  @Column(name = "date_inv")
+	LocalDateTime dateInventaire;
+
+  @Column(name = "qte_inv")
+	Integer quantiteInventaire;
+
+  @Column(name = "esp_code")
+	String codeEspece;
+
+  @Column(name = "cen_ref")
+	String referenceEntrepot;
+
+  @Column(name = "sum_qtt")
+	Integer sommeQuantiteInventaire;
+
+}
--- src/main/java/fr/microtec/geo2/persistance/repository/tiers/GeoEntrepotRepository.java
+++ src/main/java/fr/microtec/geo2/persistance/repository/tiers/GeoEntrepotRepository.java
@@ -1,10 +1,64 @@
 package fr.microtec.geo2.persistance.repository.tiers;
 
 import fr.microtec.geo2.persistance.entity.tiers.GeoEntrepot;
+import fr.microtec.geo2.persistance.entity.tiers.GeoMouvementEntrepot;
+import fr.microtec.geo2.persistance.entity.tiers.GeoMouvementFournisseur;
+import fr.microtec.geo2.persistance.entity.tiers.GeoRecapitulatifEntrepot;
+import fr.microtec.geo2.persistance.entity.tiers.GeoRecapitulatifFournisseur;
 import fr.microtec.geo2.persistance.repository.GeoRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GeoEntrepotRepository extends GeoRepository<GeoEntrepot, String> {
+
+  @Query(
+    name = "Entrepot.allMouvementEntrepot",
+    nativeQuery = true
+  )
+  List<GeoMouvementEntrepot> allMouvementEntrepot(
+    @Param("arg_dat_max") LocalDateTime dateMin,
+    @Param("arg_soc_code") String codeSociete,
+    @Param("arg_cen_ref") String codeEntrepot,
+    @Param("arg_per_code_com") String codeCommercial
+  );
+
+  @Query(
+    name = "Entrepot.allMouvementFournisseur",
+    nativeQuery = true
+  )
+  List<GeoMouvementFournisseur> allMouvementFournisseur(
+    @Param("arg_dat_max") LocalDateTime dateMin,
+    @Param("arg_soc_code") String codeSociete,
+    @Param("arg_cen_ref") String codeEntrepot,
+    @Param("arg_per_code_com") String codeCommercial
+  );
+
+  @Query(
+    name = "Entrepot.allRecapitulatifEntrepot",
+    nativeQuery = true
+  )
+  List<GeoRecapitulatifEntrepot> allRecapitulatifEntrepot(
+    @Param("arg_dat_max") LocalDateTime dateMin,
+    @Param("arg_soc_code") String codeSociete,
+    @Param("arg_cen_ref") String codeEntrepot,
+    @Param("arg_per_code_com") String codeCommercial
+  );
+
+  @Query(
+    name = "Entrepot.allRecapitulatifFournisseur",
+    nativeQuery = true
+  )
+  List<GeoRecapitulatifFournisseur> allRecapitulatifFournisseur(
+    @Param("arg_dat_max") LocalDateTime dateMin,
+    @Param("arg_soc_code") String codeSociete,
+    @Param("arg_cen_ref") String codeEntrepot,
+    @Param("arg_per_code_com") String codeCommercial
+  );
 
 }
