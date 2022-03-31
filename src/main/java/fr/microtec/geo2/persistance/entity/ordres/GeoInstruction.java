0a84518bc0b7a96482ffea88f5bd7006c29bb9e4
Thomas <thomas@microtec.fr>
2022-03-31 17:26:59 +0200

    add `GeoInstructionEntity`

---
+++ src/main/java/fr/microtec/geo2/persistance/entity/ordres/GeoInstruction.java
@@ -0,0 +1,25 @@
+package fr.microtec.geo2.persistance.entity.ordres;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import fr.microtec.geo2.persistance.entity.ValidateAndModifiedEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "geo_instruc")
+@Entity
+public class GeoInstruction extends ValidateAndModifiedEntity {
+
+    @Id
+    @Column(name = "ins_code")
+    private String id;
+
+    @Column(name = "ins_desc")
+    private String description;
+
+}
