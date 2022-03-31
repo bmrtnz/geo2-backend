6161abd59a4eb2e7beb42bc44fb39cd2157b9cdb
Thomas <thomas@microtec.fr>
2022-03-31 17:30:15 +0200

    add `GeoInstruction` GQL endpoint

---
+++ src/main/java/fr/microtec/geo2/service/graphql/ordres/GeoInstructionGraphQLService.java
@@ -0,0 +1,43 @@
+package fr.microtec.geo2.service.graphql.ordres;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Service;
+
+import fr.microtec.geo2.configuration.graphql.RelayPage;
+import fr.microtec.geo2.persistance.entity.ordres.GeoInstruction;
+import fr.microtec.geo2.persistance.repository.ordres.GeoInstructionRepository;
+import fr.microtec.geo2.service.graphql.GeoAbstractGraphQLService;
+import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLEnvironment;
+import io.leangen.graphql.annotations.GraphQLNonNull;
+import io.leangen.graphql.annotations.GraphQLQuery;
+import io.leangen.graphql.execution.ResolutionEnvironment;
+import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
+
+@Service
+@GraphQLApi
+@Secured("ROLE_USER")
+public class GeoInstructionGraphQLService extends GeoAbstractGraphQLService<GeoInstruction, String> {
+
+	public GeoInstructionGraphQLService(GeoInstructionRepository repository) {
+		super(repository, GeoInstruction.class);
+	}
+
+	@GraphQLQuery
+	public RelayPage<GeoInstruction> allInstruction(
+			@GraphQLArgument(name = "search") String search,
+			@GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
+			@GraphQLEnvironment ResolutionEnvironment env) {
+		return this.getPage(search, pageable, env);
+	}
+
+	@GraphQLQuery
+	public Optional<GeoInstruction> getInstruction(
+			@GraphQLArgument(name = "id") String id) {
+		return super.getOne(id);
+	}
+
+}
