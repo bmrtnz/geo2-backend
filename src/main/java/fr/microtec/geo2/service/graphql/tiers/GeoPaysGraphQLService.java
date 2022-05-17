bc1bda6173b2b9547c8da94059e4447713cc339e
Thomas <thomas@microtec.fr>
2022-05-17 09:31:29 +0200

    add new `depassement en cours` GQL endpoint #16718

--- src/main/java/fr/microtec/geo2/service/graphql/tiers/GeoPaysGraphQLService.java
+++ src/main/java/fr/microtec/geo2/service/graphql/tiers/GeoPaysGraphQLService.java
@@ -1,5 +1,6 @@
 package fr.microtec.geo2.service.graphql.tiers;
 
+import java.util.List;
 import java.util.Optional;
 
 import io.leangen.graphql.annotations.*;
@@ -23,9 +24,8 @@ public class GeoPaysGraphQLService extends GeoAbstractGraphQLService<GeoPays, St
 	private final PaysService paysService;
 
 	public GeoPaysGraphQLService(
-		GeoPaysRepository repository,
-		PaysService paysService
-	) {
+			GeoPaysRepository repository,
+			PaysService paysService) {
 		super(repository, GeoPays.class);
 		this.paysService = paysService;
 	}
@@ -34,72 +34,75 @@ public class GeoPaysGraphQLService extends GeoAbstractGraphQLService<GeoPays, St
 	public RelayPage<GeoPays> allPays(
 			@GraphQLArgument(name = "search") String search,
 			@GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
-			@GraphQLEnvironment ResolutionEnvironment env
-	) {
+			@GraphQLEnvironment ResolutionEnvironment env) {
 		return this.getPage(search, pageable, env);
 	}
 
 	@GraphQLQuery
 	public RelayPage<GeoPays> allDistinctPays(
 			@GraphQLArgument(name = "search") String search,
-			@GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable
-	) {
+			@GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable) {
 		return this.paysService.fetchDistinctPays(search, pageable);
 	}
 
 	@GraphQLQuery
+	public List<GeoPays> allPaysList(
+			@GraphQLArgument(name = "search") String search,
+			@GraphQLEnvironment ResolutionEnvironment env) {
+		return this.getAll(search);
+	}
+
+	@GraphQLQuery
 	public Optional<GeoPays> getPays(
-			@GraphQLArgument(name = "id") String id
-	) {
+			@GraphQLArgument(name = "id") String id) {
 		return super.getOne(id);
 	}
-	
+
 	@GraphQLQuery
 	public long countPays(
-		@GraphQLArgument(name = "search") String search
-	) {
+			@GraphQLArgument(name = "search") String search) {
 		return this.count(search);
 	}
 
 	@GraphQLQuery
 	public Float clientsSommeAgrement(@GraphQLContext GeoPays pays) {
 		return this.paysService
-		.fetchSum(pays, "clients.agrement").floatValue();
+				.fetchSum(pays, "clients.agrement").floatValue();
 	}
 
 	@GraphQLQuery
 	public Float clientsSommeEnCoursTemporaire(@GraphQLContext GeoPays pays) {
 		return this.paysService
-		.fetchSum(pays, "clients.enCoursTemporaire").floatValue();
+				.fetchSum(pays, "clients.enCoursTemporaire").floatValue();
 	}
 
 	@GraphQLQuery
 	public Float clientsSommeEnCoursBlueWhale(@GraphQLContext GeoPays pays) {
 		return this.paysService
-		.fetchSum(pays, "clients.enCoursBlueWhale").floatValue();
+				.fetchSum(pays, "clients.enCoursBlueWhale").floatValue();
 	}
 
 	@GraphQLQuery
 	public Float clientsSommeAutorise(@GraphQLContext GeoPays pays) {
 		Float agrement = this.paysService
-		.fetchSum(pays, "clients.agrement").floatValue();
+				.fetchSum(pays, "clients.agrement").floatValue();
 		Float enCoursTemp = this.paysService
-		.fetchSum(pays, "clients.enCoursTemporaire").floatValue();
+				.fetchSum(pays, "clients.enCoursTemporaire").floatValue();
 		Float enCoursBW = this.paysService
-		.fetchSum(pays, "clients.enCoursBlueWhale").floatValue();
+				.fetchSum(pays, "clients.enCoursBlueWhale").floatValue();
 		return agrement + enCoursTemp + enCoursBW;
 	}
 
 	@GraphQLQuery
 	public Float clientsSommeDepassement(@GraphQLContext GeoPays pays) {
 		Float agrement = this.paysService
-		.fetchSum(pays, "clients.agrement").floatValue();
+				.fetchSum(pays, "clients.agrement").floatValue();
 		Float enCoursTemp = this.paysService
-		.fetchSum(pays, "clients.enCoursTemporaire").floatValue();
+				.fetchSum(pays, "clients.enCoursTemporaire").floatValue();
 		Float enCoursBW = this.paysService
-		.fetchSum(pays, "clients.enCoursBlueWhale").floatValue();
+				.fetchSum(pays, "clients.enCoursBlueWhale").floatValue();
 		Float enCoursActuel = this.paysService
-		.fetchSum(pays, "clients.enCoursActuel").floatValue();
+				.fetchSum(pays, "clients.enCoursActuel").floatValue();
 		Float enCoursTotal = agrement + enCoursTemp + enCoursBW;
 		Float depassement = enCoursActuel - enCoursTotal;
 		return depassement > 0 ? depassement : 0f;
@@ -108,43 +111,43 @@ public class GeoPaysGraphQLService extends GeoAbstractGraphQLService<GeoPays, St
 	@GraphQLQuery
 	public Float clientsSommeEnCoursActuel(@GraphQLContext GeoPays pays) {
 		return this.paysService
-		.fetchSum(pays, "clients.enCoursActuel").floatValue();
+				.fetchSum(pays, "clients.enCoursActuel").floatValue();
 	}
 
 	@GraphQLQuery
 	public Float clientsSommeEnCoursNonEchu(@GraphQLContext GeoPays pays) {
 		return this.paysService
-		.fetchSum(pays, "clients.enCoursNonEchu").floatValue();
+				.fetchSum(pays, "clients.enCoursNonEchu").floatValue();
 	}
 
 	@GraphQLQuery
 	public Float clientsSommeEnCours1a30(@GraphQLContext GeoPays pays) {
 		return this.paysService
-		.fetchSum(pays, "clients.enCours1a30").floatValue();
+				.fetchSum(pays, "clients.enCours1a30").floatValue();
 	}
 
 	@GraphQLQuery
 	public Float clientsSommeEnCours31a60(@GraphQLContext GeoPays pays) {
 		return this.paysService
-		.fetchSum(pays, "clients.enCours31a60").floatValue();
+				.fetchSum(pays, "clients.enCours31a60").floatValue();
 	}
 
 	@GraphQLQuery
 	public Float clientsSommeEnCours61a90(@GraphQLContext GeoPays pays) {
 		return this.paysService
-		.fetchSum(pays, "clients.enCours61a90").floatValue();
+				.fetchSum(pays, "clients.enCours61a90").floatValue();
 	}
 
 	@GraphQLQuery
 	public Float clientsSommeEnCours90Plus(@GraphQLContext GeoPays pays) {
 		return this.paysService
-		.fetchSum(pays, "clients.enCours90Plus").floatValue();
+				.fetchSum(pays, "clients.enCours90Plus").floatValue();
 	}
 
 	@GraphQLQuery
 	public Float clientsSommeAlerteCoface(@GraphQLContext GeoPays pays) {
 		return this.paysService
-		.fetchSum(pays, "clients.alerteCoface").floatValue();
+				.fetchSum(pays, "clients.alerteCoface").floatValue();
 	}
 
 }
