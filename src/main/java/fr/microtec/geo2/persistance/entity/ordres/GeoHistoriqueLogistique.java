3a873355fce77defd8d44d409cfe7a83986be60f
Thomas <thomas@microtec.fr>
2022-05-18 11:56:09 +0200

    add `HistoriqueLogistique` data stack #16647

---
+++ src/main/java/fr/microtec/geo2/persistance/entity/ordres/GeoHistoriqueLogistique.java
@@ -0,0 +1,36 @@
+package fr.microtec.geo2.persistance.entity.ordres;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import fr.microtec.geo2.persistance.entity.ModifiedEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Table(name = "geo_histo_ordlog_declo")
+@Entity
+public class GeoHistoriqueLogistique extends ModifiedEntity {
+
+    @Id
+    @Column(name = "histo_orx_ref")
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "orx_ref", nullable = false)
+    private GeoOrdreLogistique logistique;
+
+    @Column(name = "flag_exped_fournni")
+    private Boolean expedieStation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ref_devalexp", nullable = false)
+    private GeoCodifDevalexp devalexp;
+
+}
---
+++ src/main/java/fr/microtec/geo2/service/graphql/ordres/GeoHistoriqueLogistiqueGraphQLService.java
@@ -0,0 +1,43 @@
+package fr.microtec.geo2.service.graphql.ordres;
+
+import java.util.Optional;
+
+import io.leangen.graphql.annotations.GraphQLEnvironment;
+import io.leangen.graphql.execution.ResolutionEnvironment;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Service;
+
+import fr.microtec.geo2.configuration.graphql.RelayPage;
+import fr.microtec.geo2.persistance.entity.ordres.GeoHistoriqueLogistique;
+import fr.microtec.geo2.persistance.repository.ordres.GeoHistoriqueLogistiqueRepository;
+import fr.microtec.geo2.service.graphql.GeoAbstractGraphQLService;
+import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLNonNull;
+import io.leangen.graphql.annotations.GraphQLQuery;
+import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
+
+@Service
+@GraphQLApi
+@Secured("ROLE_USER")
+public class GeoHistoriqueLogistiqueGraphQLService extends GeoAbstractGraphQLService<GeoHistoriqueLogistique, String> {
+
+	public GeoHistoriqueLogistiqueGraphQLService(GeoHistoriqueLogistiqueRepository repository) {
+		super(repository, GeoHistoriqueLogistique.class);
+	}
+
+	@GraphQLQuery
+	public RelayPage<GeoHistoriqueLogistique> allHistoriqueLogistique(
+			@GraphQLArgument(name = "search") String search,
+			@GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
+			@GraphQLEnvironment ResolutionEnvironment env) {
+		return this.getPage(search, pageable, env);
+	}
+
+	@GraphQLQuery
+	public Optional<GeoHistoriqueLogistique> getHistoriqueLogistique(
+			@GraphQLArgument(name = "id") String id) {
+		return super.getOne(id);
+	}
+
+}
