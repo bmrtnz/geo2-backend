b6208d43a89d5607e8f070cb38a92c5a94f4f1ed
Thomas <thomas@microtec.fr>
2022-06-02 17:08:00 +0200

    fix missing fields completion on contact insertion #16994

--- src/main/java/fr/microtec/geo2/persistance/repository/tiers/GeoEntrepotRepository.java
+++ src/main/java/fr/microtec/geo2/persistance/repository/tiers/GeoEntrepotRepository.java
@@ -2,6 +2,7 @@ package fr.microtec.geo2.persistance.repository.tiers;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -17,36 +18,37 @@ import fr.microtec.geo2.persistance.repository.GeoRepository;
 @Repository
 public interface GeoEntrepotRepository extends GeoRepository<GeoEntrepot, String> {
 
-  @Query(name = "Entrepot.allMouvementEntrepot", nativeQuery = true)
-  List<GeoMouvementEntrepot> allMouvementEntrepot(
-      @Param("arg_dat_max") LocalDateTime dateMaxMouvements,
-      @Param("arg_soc_code") String codeSociete,
-      @Param("arg_cen_ref") String codeEntrepot,
-      @Param("arg_per_code_com") String codeCommercial,
-      @Param("arg_fou_code") String codeFournisseur);
+    @Query(name = "Entrepot.allMouvementEntrepot", nativeQuery = true)
+    List<GeoMouvementEntrepot> allMouvementEntrepot(
+            @Param("arg_dat_max") LocalDateTime dateMaxMouvements,
+            @Param("arg_soc_code") String codeSociete,
+            @Param("arg_cen_ref") String codeEntrepot,
+            @Param("arg_per_code_com") String codeCommercial,
+            @Param("arg_fou_code") String codeFournisseur);
 
-  @Query(name = "Entrepot.allMouvementFournisseur", nativeQuery = true)
-  List<GeoMouvementFournisseur> allMouvementFournisseur(
-      @Param("arg_dat_max") LocalDateTime dateMaxMouvements,
-      @Param("arg_soc_code") String codeSociete,
-      @Param("arg_cen_ref") String codeEntrepot,
-      @Param("arg_per_code_com") String codeCommercial,
-      @Param("arg_fou_code") String codeFournisseur);
+    @Query(name = "Entrepot.allMouvementFournisseur", nativeQuery = true)
+    List<GeoMouvementFournisseur> allMouvementFournisseur(
+            @Param("arg_dat_max") LocalDateTime dateMaxMouvements,
+            @Param("arg_soc_code") String codeSociete,
+            @Param("arg_cen_ref") String codeEntrepot,
+            @Param("arg_per_code_com") String codeCommercial,
+            @Param("arg_fou_code") String codeFournisseur);
 
-  @Query(name = "Entrepot.allRecapitulatifEntrepot", nativeQuery = true)
-  List<GeoRecapitulatifEntrepot> allRecapitulatifEntrepot(
-      @Param("arg_dat_max") LocalDateTime dateMaxMouvements,
-      @Param("arg_soc_code") String codeSociete,
-      @Param("arg_cen_ref") String codeEntrepot,
-      @Param("arg_per_code_com") String codeCommercial,
-      @Param("arg_fou_code") String codeFournisseur);
+    @Query(name = "Entrepot.allRecapitulatifEntrepot", nativeQuery = true)
+    List<GeoRecapitulatifEntrepot> allRecapitulatifEntrepot(
+            @Param("arg_dat_max") LocalDateTime dateMaxMouvements,
+            @Param("arg_soc_code") String codeSociete,
+            @Param("arg_cen_ref") String codeEntrepot,
+            @Param("arg_per_code_com") String codeCommercial,
+            @Param("arg_fou_code") String codeFournisseur);
 
-  @Query(name = "Entrepot.allRecapitulatifFournisseur", nativeQuery = true)
-  List<GeoRecapitulatifFournisseur> allRecapitulatifFournisseur(
-      @Param("arg_dat_max") LocalDateTime dateMaxMouvements,
-      @Param("arg_soc_code") String codeSociete,
-      @Param("arg_cen_ref") String codeEntrepot,
-      @Param("arg_per_code_com") String codeCommercial,
-      @Param("arg_fou_code") String codeFournisseur);
+    @Query(name = "Entrepot.allRecapitulatifFournisseur", nativeQuery = true)
+    List<GeoRecapitulatifFournisseur> allRecapitulatifFournisseur(
+            @Param("arg_dat_max") LocalDateTime dateMaxMouvements,
+            @Param("arg_soc_code") String codeSociete,
+            @Param("arg_cen_ref") String codeEntrepot,
+            @Param("arg_per_code_com") String codeCommercial,
+            @Param("arg_fou_code") String codeFournisseur);
 
+    Optional<GeoEntrepot> getOneByCode(String code);
 }
