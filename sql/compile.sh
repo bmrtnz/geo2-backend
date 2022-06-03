922313a9d3325c8e4560522e6a6b2c7c672fa1c9
valentin <valentin@microtec.fr>
2022-06-03 10:45:03 +0200

    add editorconfig and fix sql compilation

--- sql/compile.sh
+++ sql/compile.sh
@@ -2,4 +2,8 @@
 
 OUT=sql/output.sql
 [[ -f $OUT ]] && rm $OUT
-cat sql/*.sql > $OUT
+#cat sql/*.sql > $OUT
+for f in sql/*.sql; do
+  cat $f >> $OUT
+  echo '' >> $OUT
+done
--- sql/f_split.sql
+++ sql/f_split.sql
@@ -32,3 +32,4 @@ BEGIN
         end loop;
 end;
 /
+
