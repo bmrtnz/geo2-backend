96b00dcbd11e6cc8318e4fdf65536db66f74339b
Thomas <thomas@microtec.fr>
2022-06-08 17:47:57 +0200

    add `StockArticle` entity `quantiteCalculee` fields #16979

--- src/main/java/fr/microtec/geo2/service/graphql/stock/GeoStockGraphQLService.java
+++ src/main/java/fr/microtec/geo2/service/graphql/stock/GeoStockGraphQLService.java
@@ -13,6 +13,7 @@ import fr.microtec.geo2.persistance.entity.stock.GeoStockArticle;
 import fr.microtec.geo2.persistance.repository.stock.GeoStockRepository;
 import fr.microtec.geo2.service.graphql.GeoAbstractGraphQLService;
 import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLContext;
 import io.leangen.graphql.annotations.GraphQLEnvironment;
 import io.leangen.graphql.annotations.GraphQLNonNull;
 import io.leangen.graphql.annotations.GraphQLQuery;
@@ -24,40 +25,68 @@ import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 @Secured("ROLE_USER")
 public class GeoStockGraphQLService extends GeoAbstractGraphQLService<GeoStock, String> {
 
-	public GeoStockGraphQLService(GeoStockRepository stockRepository) {
-		super(stockRepository, GeoStock.class);
-	}
+    public GeoStockGraphQLService(GeoStockRepository stockRepository) {
+        super(stockRepository, GeoStock.class);
+    }
 
-	@GraphQLQuery
-	public RelayPage<GeoStock> allStock(
-			@GraphQLArgument(name = "search") String search,
-			@GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
-			@GraphQLEnvironment ResolutionEnvironment env) {
-		return this.getPage(search, pageable, env);
-	}
+    @GraphQLQuery
+    public RelayPage<GeoStock> allStock(
+            @GraphQLArgument(name = "search") String search,
+            @GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
+            @GraphQLEnvironment ResolutionEnvironment env) {
+        return this.getPage(search, pageable, env);
+    }
 
-	@GraphQLQuery
-	public Optional<GeoStock> getStock(
-			@GraphQLArgument(name = "id") String id) {
-		return this.getOne(id);
-	}
+    @GraphQLQuery
+    public Optional<GeoStock> getStock(
+            @GraphQLArgument(name = "id") String id) {
+        return this.getOne(id);
+    }
 
-	@GraphQLQuery
-	public List<GeoStockArticle> allStockArticleList(
-			@GraphQLArgument(name = "espece") String espece,
-			@GraphQLArgument(name = "variete", defaultValue = "%") String variete,
-			@GraphQLArgument(name = "origine", defaultValue = "%") String origine,
-			@GraphQLArgument(name = "modeCulture", defaultValue = "%") String modeCulture,
-			@GraphQLArgument(name = "emballage", defaultValue = "%") String emballage,
-			@GraphQLArgument(name = "bureauAchat", defaultValue = "%") String bureauAchat) {
-		return ((GeoStockRepository) this.repository)
-				.allStockArticleList(
-						espece,
-						Optional.ofNullable(variete).orElse("%"),
-						Optional.ofNullable(origine).orElse("%"),
-						Optional.ofNullable(modeCulture).orElse("%"),
-						Optional.ofNullable(emballage).orElse("%"),
-						Optional.ofNullable(bureauAchat).orElse("%"));
-	}
+    @GraphQLQuery
+    public List<GeoStockArticle> allStockArticleList(
+            @GraphQLArgument(name = "espece") String espece,
+            @GraphQLArgument(name = "variete", defaultValue = "%") String variete,
+            @GraphQLArgument(name = "origine", defaultValue = "%") String origine,
+            @GraphQLArgument(name = "modeCulture", defaultValue = "%") String modeCulture,
+            @GraphQLArgument(name = "emballage", defaultValue = "%") String emballage,
+            @GraphQLArgument(name = "bureauAchat", defaultValue = "%") String bureauAchat) {
+        return ((GeoStockRepository) this.repository)
+                .allStockArticleList(
+                        espece,
+                        Optional.ofNullable(variete).orElse("%"),
+                        Optional.ofNullable(origine).orElse("%"),
+                        Optional.ofNullable(modeCulture).orElse("%"),
+                        Optional.ofNullable(emballage).orElse("%"),
+                        Optional.ofNullable(bureauAchat).orElse("%"));
+    }
+
+    @GraphQLQuery
+    public Float quantiteCalculee1(@GraphQLContext GeoStockArticle stockArticle) {
+        return stockArticle.getQuantiteInitiale1()
+                - stockArticle.getQuantiteReservee1()
+                - stockArticle.getQuantiteOptionnelle1();
+    }
+
+    @GraphQLQuery
+    public Float quantiteCalculee2(@GraphQLContext GeoStockArticle stockArticle) {
+        return stockArticle.getQuantiteInitiale2()
+                - stockArticle.getQuantiteReservee2()
+                - stockArticle.getQuantiteOptionnelle2();
+    }
+
+    @GraphQLQuery
+    public Float quantiteCalculee3(@GraphQLContext GeoStockArticle stockArticle) {
+        return stockArticle.getQuantiteInitiale3()
+                - stockArticle.getQuantiteReservee3()
+                - stockArticle.getQuantiteOptionnelle3();
+    }
+
+    @GraphQLQuery
+    public Float quantiteCalculee4(@GraphQLContext GeoStockArticle stockArticle) {
+        return stockArticle.getQuantiteInitiale4()
+                - stockArticle.getQuantiteReservee4()
+                - stockArticle.getQuantiteOptionnelle4();
+    }
 
 }
