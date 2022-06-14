448f4ddb4bb7a1c3b7013b9726dd19a7163009c2
Thomas <thomas@microtec.fr>
2022-06-14 16:04:36 +0200

    fix unwanted `ordre-ligne` decloturation #17084

--- src/main/java/fr/microtec/geo2/service/graphql/ordres/GeoOrdreLogistiqueGraphQLService.java
+++ src/main/java/fr/microtec/geo2/service/graphql/ordres/GeoOrdreLogistiqueGraphQLService.java
@@ -2,9 +2,6 @@ package fr.microtec.geo2.service.graphql.ordres;
 
 import java.util.Optional;
 
-import io.leangen.graphql.annotations.GraphQLEnvironment;
-import io.leangen.graphql.annotations.GraphQLMutation;
-import io.leangen.graphql.execution.ResolutionEnvironment;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
@@ -15,8 +12,11 @@ import fr.microtec.geo2.persistance.repository.ordres.GeoOrdreLogistiqueReposito
 import fr.microtec.geo2.service.OrdreLogistiqueService;
 import fr.microtec.geo2.service.graphql.GeoAbstractGraphQLService;
 import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLEnvironment;
+import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLNonNull;
 import io.leangen.graphql.annotations.GraphQLQuery;
+import io.leangen.graphql.execution.ResolutionEnvironment;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 
 @Service
@@ -24,37 +24,37 @@ import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 @Secured("ROLE_USER")
 public class GeoOrdreLogistiqueGraphQLService extends GeoAbstractGraphQLService<GeoOrdreLogistique, String> {
 
-	private final OrdreLogistiqueService ordreLogistiqueService;
+    private final OrdreLogistiqueService ordreLogistiqueService;
 
-	public GeoOrdreLogistiqueGraphQLService(GeoOrdreLogistiqueRepository repository,
-			OrdreLogistiqueService ordreLogistiqueService) {
-		super(repository, GeoOrdreLogistique.class);
-		this.ordreLogistiqueService = ordreLogistiqueService;
-	}
+    public GeoOrdreLogistiqueGraphQLService(GeoOrdreLogistiqueRepository repository,
+            OrdreLogistiqueService ordreLogistiqueService) {
+        super(repository, GeoOrdreLogistique.class);
+        this.ordreLogistiqueService = ordreLogistiqueService;
+    }
 
-	@GraphQLQuery
-	public RelayPage<GeoOrdreLogistique> allOrdreLogistique(
-			@GraphQLArgument(name = "search") String search,
-			@GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
-			@GraphQLEnvironment ResolutionEnvironment env) {
-		return this.getPage(search, pageable, env);
-	}
+    @GraphQLQuery
+    public RelayPage<GeoOrdreLogistique> allOrdreLogistique(
+            @GraphQLArgument(name = "search") String search,
+            @GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
+            @GraphQLEnvironment ResolutionEnvironment env) {
+        return this.getPage(search, pageable, env);
+    }
 
-	@GraphQLQuery
-	public Optional<GeoOrdreLogistique> getOrdreLogistique(
-			@GraphQLArgument(name = "id") String id) {
-		return super.getOne(id);
-	}
+    @GraphQLQuery
+    public Optional<GeoOrdreLogistique> getOrdreLogistique(
+            @GraphQLArgument(name = "id") String id) {
+        return super.getOne(id);
+    }
 
-	@GraphQLMutation
-	public GeoOrdreLogistique saveOrdreLogistique(GeoOrdreLogistique ordreLogistique,
-			@GraphQLEnvironment ResolutionEnvironment env) {
-		return this.saveEntity(this.ordreLogistiqueService.withDefaults(ordreLogistique), env);
-	}
+    @GraphQLMutation
+    public GeoOrdreLogistique saveOrdreLogistique(GeoOrdreLogistique ordreLogistique,
+            @GraphQLEnvironment ResolutionEnvironment env) {
+        return this.saveEntity(ordreLogistique, env);
+    }
 
-	@GraphQLMutation
-	public boolean deleteOrdreLogistique(String id) {
-		return this.delete(id);
-	}
+    @GraphQLMutation
+    public boolean deleteOrdreLogistique(String id) {
+        return this.delete(id);
+    }
 
 }
