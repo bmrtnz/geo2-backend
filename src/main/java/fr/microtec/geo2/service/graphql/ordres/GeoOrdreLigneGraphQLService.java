df56e53ae6c2a3f3f223af38b077d93bc3d9452a
Thomas <thomas@microtec.fr>
2022-06-16 17:13:43 +0200

    ondoing grid commandes rebuild #17039

--- src/main/java/fr/microtec/geo2/service/graphql/ordres/GeoOrdreLigneGraphQLService.java
+++ src/main/java/fr/microtec/geo2/service/graphql/ordres/GeoOrdreLigneGraphQLService.java
@@ -1,5 +1,18 @@
 package fr.microtec.geo2.service.graphql.ordres;
 
+import static fr.microtec.geo2.persistance.entity.FunctionResult.RESULT_UNKNOWN;
+
+import java.lang.reflect.Field;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import org.springframework.util.ReflectionUtils;
+
 import fr.microtec.geo2.configuration.graphql.RelayPage;
 import fr.microtec.geo2.persistance.entity.FunctionResult;
 import fr.microtec.geo2.persistance.entity.ordres.GeoOrdreLigne;
@@ -9,22 +22,14 @@ import fr.microtec.geo2.service.OrdreLigneService;
 import fr.microtec.geo2.service.graphql.GeoAbstractGraphQLService;
 import fr.microtec.geo2.service.security.SecurityService;
 import graphql.GraphQLException;
-import io.leangen.graphql.annotations.*;
+import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLEnvironment;
+import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLNonNull;
+import io.leangen.graphql.annotations.GraphQLQuery;
 import io.leangen.graphql.execution.ResolutionEnvironment;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
-import org.springframework.util.ReflectionUtils;
-
-import java.lang.reflect.Field;
-import java.util.List;
-import java.util.Optional;
-import java.util.concurrent.atomic.AtomicReference;
-
-import static fr.microtec.geo2.persistance.entity.FunctionResult.RESULT_UNKNOWN;
 
 @Service
 @GraphQLApi
@@ -33,85 +38,87 @@ import static fr.microtec.geo2.persistance.entity.FunctionResult.RESULT_UNKNOWN;
 public class GeoOrdreLigneGraphQLService extends GeoAbstractGraphQLService<GeoOrdreLigne, String> {
 
     private final GeoFunctionOrdreRepository geoFunctionOrdreRepository;
-	private final OrdreLigneService ordreLigneService;
+    private final OrdreLigneService ordreLigneService;
     private final SecurityService securityService;
 
-	public GeoOrdreLigneGraphQLService(
-        GeoOrdreLigneRepository repository,
-        GeoFunctionOrdreRepository geoFunctionOrdreRepository, OrdreLigneService ordreLigneService, SecurityService securityService) {
-		super(repository, GeoOrdreLigne.class);
+    public GeoOrdreLigneGraphQLService(
+            GeoOrdreLigneRepository repository,
+            GeoFunctionOrdreRepository geoFunctionOrdreRepository, OrdreLigneService ordreLigneService,
+            SecurityService securityService) {
+        super(repository, GeoOrdreLigne.class);
         this.geoFunctionOrdreRepository = geoFunctionOrdreRepository;
         this.ordreLigneService = ordreLigneService;
         this.securityService = securityService;
     }
 
-	@GraphQLQuery
-	public RelayPage<GeoOrdreLigne> allOrdreLigne(
-			@GraphQLArgument(name = "search") String search,
-			@GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
-			@GraphQLEnvironment ResolutionEnvironment env) {
-		return this.getPage(search, pageable, env);
-	}
+    @GraphQLQuery
+    public RelayPage<GeoOrdreLigne> allOrdreLigne(
+            @GraphQLArgument(name = "search") String search,
+            @GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
+            @GraphQLEnvironment ResolutionEnvironment env) {
+        return this.getPage(search, pageable, env);
+    }
 
-	@GraphQLQuery
-	public List<GeoOrdreLigne> allOrdreLigneList(
-			@GraphQLArgument(name = "search") String search,
-			@GraphQLEnvironment ResolutionEnvironment env) {
-		return this.getAll(search);
-	}
+    @GraphQLQuery
+    public List<GeoOrdreLigne> allOrdreLigneList(
+            @GraphQLArgument(name = "search") String search,
+            @GraphQLEnvironment ResolutionEnvironment env) {
+        return this.getAll(search);
+    }
 
-	@GraphQLQuery
-	public RelayPage<GeoOrdreLigne> allOrdreLigneMarge(
-			@GraphQLArgument(name = "search") String search,
-			@GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
-			@GraphQLEnvironment ResolutionEnvironment env) {
-		return this.ordreLigneService.fetchAllMarge(search, pageable, env);
-	}
+    @GraphQLQuery
+    public RelayPage<GeoOrdreLigne> allOrdreLigneMarge(
+            @GraphQLArgument(name = "search") String search,
+            @GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
+            @GraphQLEnvironment ResolutionEnvironment env) {
+        return this.ordreLigneService.fetchAllMarge(search, pageable, env);
+    }
 
-	@GraphQLQuery
-	public RelayPage<GeoOrdreLigne> allOrdreLigneTotaux(
-			@GraphQLArgument(name = "search") String search,
-			@GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
-			@GraphQLEnvironment ResolutionEnvironment env) {
-		return this.ordreLigneService.fetchOrdreLignesTotauxDetail(search, pageable, env);
-	}
+    @GraphQLQuery
+    public RelayPage<GeoOrdreLigne> allOrdreLigneTotaux(
+            @GraphQLArgument(name = "search") String search,
+            @GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
+            @GraphQLEnvironment ResolutionEnvironment env) {
+        return this.ordreLigneService.fetchOrdreLignesTotauxDetail(search, pageable, env);
+    }
 
-	@GraphQLQuery
-	public RelayPage<GeoOrdreLigne> allOrdreLigneTotauxDetail(
-			@GraphQLArgument(name = "search") String search,
-			@GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
-			@GraphQLEnvironment ResolutionEnvironment env) {
-		return this.ordreLigneService.fetchOrdreLignesTotauxDetail(search, pageable, env);
-	}
+    @GraphQLQuery
+    public RelayPage<GeoOrdreLigne> allOrdreLigneTotauxDetail(
+            @GraphQLArgument(name = "search") String search,
+            @GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
+            @GraphQLEnvironment ResolutionEnvironment env) {
+        return this.ordreLigneService.fetchOrdreLignesTotauxDetail(search, pageable, env);
+    }
 
-	@GraphQLQuery
-	public Optional<GeoOrdreLigne> getOrdreLigne(
-			@GraphQLArgument(name = "id") String id) {
-		return super.getOne(id);
-	}
-
-	@GraphQLMutation
-	public GeoOrdreLigne saveOrdreLigne(GeoOrdreLigne ordreLigne, @GraphQLEnvironment ResolutionEnvironment env) {
-		return this.saveEntity(this.ordreLigneService.withDefaults(ordreLigne), env);
-	}
-
-	@GraphQLMutation
-	public boolean deleteOrdreLigne(String id) {
-		return this.delete(id);
-	}
-
+    @GraphQLQuery
+    public Optional<GeoOrdreLigne> getOrdreLigne(
+            @GraphQLArgument(name = "id") String id) {
+        return super.getOne(id);
+    }
 
     @GraphQLMutation
-    public GeoOrdreLigne updateField(final String fieldName, final String id, final Object value, final String socCode) {
+    public GeoOrdreLigne saveOrdreLigne(GeoOrdreLigne ordreLigne, @GraphQLEnvironment ResolutionEnvironment env) {
+        return this.saveEntity(this.ordreLigneService.withDefaults(ordreLigne), env);
+    }
+
+    @GraphQLMutation
+    public boolean deleteOrdreLigne(String id) {
+        return this.delete(id);
+    }
+
+    @GraphQLMutation
+    public GeoOrdreLigne updateField(final String fieldName, final String id, final Object value,
+            final String socCode) {
 
         final AtomicReference<GeoOrdreLigne> result = new AtomicReference<>();
         final AtomicReference<Object> newValue = new AtomicReference<>(value);
 
         final Field field = ReflectionUtils.findField(GeoOrdreLigne.class, fieldName);
-        Assert.notNull(field, String.format("Le champ \"%s\" n'a pas été trouvé dans l'objet \"GeoOrdreLigne\".", fieldName));
+        Assert.notNull(field,
+                String.format("Le champ \"%s\" n'a pas été trouvé dans l'objet \"GeoOrdreLigne\".", fieldName));
         field.setAccessible(true);
 
-        if(value instanceof Number) {
+        if (value instanceof Number) {
             final Class<?> type = field.getType();
             if (type.equals(Double.class)) {
                 newValue.set(((Number) value).doubleValue());
@@ -121,74 +128,81 @@ public class GeoOrdreLigneGraphQLService extends GeoAbstractGraphQLService<GeoOr
         }
 
         this.getOne(id)
-            .ifPresent(geoOrdreLigne -> {
-                ReflectionUtils.setField(field, geoOrdreLigne, newValue.get());
+                .ifPresent(geoOrdreLigne -> {
+                    ReflectionUtils.setField(field, geoOrdreLigne, newValue.get());
+                    String scoCode = geoOrdreLigne.getOrdre().getSecteurCommercial().getId();
 
-                this.repository.save(geoOrdreLigne);
+                    this.repository.save(geoOrdreLigne);
 
-                FunctionResult functionResult = null;
-                switch (fieldName) {
-                    case "nombrePalettesCommandees":
-                        functionResult = this.geoFunctionOrdreRepository.onChangeCdeNbPal(id, socCode);
-                        break;
+                    FunctionResult functionResult = null;
+                    switch (fieldName) {
+                        case "nombrePalettesCommandees":
+                            functionResult = this.geoFunctionOrdreRepository.onChangeCdeNbPal(id, scoCode);
+                            break;
 
-                    case "nombreColisPalette":
-                        functionResult = this.geoFunctionOrdreRepository.onChangePalNbCol(id, this.securityService.getUser().getUsername());
-                        break;
+                        case "nombreColisPalette":
+                            functionResult = this.geoFunctionOrdreRepository.onChangePalNbCol(id,
+                                    this.securityService.getUser().getUsername());
+                            break;
 
-                    case "nombreColisCommandes":
-                        functionResult = this.geoFunctionOrdreRepository.onChangeCdeNbCol(id, this.securityService.getUser().getUsername());
-                        break;
+                        case "nombreColisCommandes":
+                            functionResult = this.geoFunctionOrdreRepository.onChangeCdeNbCol(id,
+                                    this.securityService.getUser().getUsername());
+                            break;
 
-                    case "proprietaireMarchandise":
-                        if(this.geoFunctionOrdreRepository.fVerifLogistiqueOrdre(id).getRes() != RESULT_UNKNOWN) {
-                            functionResult = this.geoFunctionOrdreRepository.onChangeProprCode(id, this.securityService.getUser().getUsername(), socCode);
-                        }
-                        break;
+                        case "proprietaireMarchandise":
+                            if (this.geoFunctionOrdreRepository.fVerifLogistiqueOrdre(id).getRes() != RESULT_UNKNOWN) {
+                                functionResult = this.geoFunctionOrdreRepository.onChangeProprCode(id,
+                                        this.securityService.getUser().getUsername(), socCode);
+                            }
+                            break;
 
-                    case "fournisseur":
-                        if(this.geoFunctionOrdreRepository.fVerifLogistiqueOrdre(id).getRes() != RESULT_UNKNOWN) {
-                            functionResult = this.geoFunctionOrdreRepository.onChangeFouCode(id, this.securityService.getUser().getUsername(), socCode);
-                        }
-                        break;
+                        case "fournisseur":
+                            if (this.geoFunctionOrdreRepository.fVerifLogistiqueOrdre(id).getRes() != RESULT_UNKNOWN) {
+                                functionResult = this.geoFunctionOrdreRepository.onChangeFouCode(id,
+                                        this.securityService.getUser().getUsername(), socCode);
+                            }
+                            break;
 
-                    case "ventePrixUnitaire":
-                        functionResult = this.geoFunctionOrdreRepository.onChangeVtePu(id);
-                        break;
+                        case "ventePrixUnitaire":
+                            functionResult = this.geoFunctionOrdreRepository.onChangeVtePu(id);
+                            break;
 
-                    case "gratuit":
-                        functionResult = this.geoFunctionOrdreRepository.onChangeIndGratuit(id);
-                        break;
+                        case "gratuit":
+                            functionResult = this.geoFunctionOrdreRepository.onChangeIndGratuit(id);
+                            break;
 
-                    case "achatDevisePrixUnitaire":
-                        functionResult = this.geoFunctionOrdreRepository.onChangeAchDevPu(id, socCode);
-                        break;
+                        case "achatDevisePrixUnitaire":
+                            functionResult = this.geoFunctionOrdreRepository.onChangeAchDevPu(id, socCode);
+                            break;
 
-                    case "typePalette":
-                        functionResult = this.geoFunctionOrdreRepository.onChangePalCode(id, this.securityService.getUser().getUsername(), socCode);
-                        break;
+                        case "typePalette":
+                            functionResult = this.geoFunctionOrdreRepository.onChangePalCode(id,
+                                    this.securityService.getUser().getUsername(), scoCode);
+                            break;
 
-                    case "paletteInter":
-                        functionResult = this.geoFunctionOrdreRepository.onChangePalinterCode(id);
-                        break;
+                        case "paletteInter":
+                            functionResult = this.geoFunctionOrdreRepository.onChangePalinterCode(id);
+                            break;
 
-                    case "nombrePalettesIntermediaires":
-                        functionResult = this.geoFunctionOrdreRepository.onChangePalNbPalinter(id, this.securityService.getUser().getUsername());
-                        break;
-                }
+                        case "nombrePalettesIntermediaires":
+                            functionResult = this.geoFunctionOrdreRepository.onChangePalNbPalinter(id,
+                                    this.securityService.getUser().getUsername());
+                            break;
+                    }
 
-                // Si le résultat est bon, on retourne la ligne de commande dans la réponse.
+                    // Si le résultat est bon, on retourne la ligne de commande dans la réponse.
 
-                if(functionResult == null || functionResult.getRes() == RESULT_UNKNOWN) {
-                    String msg = functionResult != null ?
-                        functionResult.getMsg() :
-                        String.format("Le champ \"%s\" n'est pas supporté !!!", fieldName);
+                    if (functionResult != null && functionResult.getRes() == RESULT_UNKNOWN) {
+                        String msg = functionResult != null ? functionResult.getMsg()
+                                : String.format("Erreur lors de la mise à jour du champ \"%s\" ( %s )", fieldName,
+                                        functionResult.getMsg());
 
-                    throw new GraphQLException(msg);
-                }
+                        throw new GraphQLException(msg);
+                    }
 
-                this.repository.findById(id).ifPresent(result::set);
-            });
+                    this.repository.findById(id).ifPresent(result::set);
+                });
 
         return result.get();
     }
