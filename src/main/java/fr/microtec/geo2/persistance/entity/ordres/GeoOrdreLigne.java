af76904d0f53995e2799c8435af78cb6b6ce2b53
Thomas <thomas@microtec.fr>
2022-07-12 16:06:52 +0200

    update `stocks` entities #17180

--- src/main/java/fr/microtec/geo2/persistance/entity/ordres/GeoOrdreLigne.java
+++ src/main/java/fr/microtec/geo2/persistance/entity/ordres/GeoOrdreLigne.java
@@ -1,6 +1,5 @@
 package fr.microtec.geo2.persistance.entity.ordres;
 
-import java.io.Serializable;
 import java.util.List;
 
 import javax.persistence.Column;
@@ -23,6 +22,7 @@ import fr.microtec.geo2.persistance.entity.ValidateAndModifiedEntity;
 import fr.microtec.geo2.persistance.entity.produits.GeoArticle;
 import fr.microtec.geo2.persistance.entity.produits.GeoEspece;
 import fr.microtec.geo2.persistance.entity.produits.GeoTemperature;
+import fr.microtec.geo2.persistance.entity.stock.GeoStockMouvement;
 import fr.microtec.geo2.persistance.entity.tiers.GeoBaseTarif;
 import fr.microtec.geo2.persistance.entity.tiers.GeoBureauAchat;
 import fr.microtec.geo2.persistance.entity.tiers.GeoFournisseur;
@@ -37,306 +37,309 @@ import lombok.EqualsAndHashCode;
 @Table(name = "geo_ordlig")
 @Entity
 @DynamicUpdate
-public class GeoOrdreLigne extends ValidateAndModifiedEntity implements Serializable {
+public class GeoOrdreLigne extends ValidateAndModifiedEntity {
 
-	@Id
-	@Column(name = "orl_ref")
-	private String id;
+    @Id
+    @Column(name = "orl_ref")
+    private String id;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "art_ref")
-	private GeoArticle article;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "art_ref")
+    private GeoArticle article;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "ord_ref")
-	private GeoOrdre ordre;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ord_ref")
+    private GeoOrdre ordre;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "ord_ref", referencedColumnName = "ord_ref", insertable = false, updatable = false)
-	@JoinColumn(name = "fou_code", referencedColumnName = "fou_code", insertable = false, updatable = false)
-	private GeoOrdreLogistique logistique;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ord_ref", referencedColumnName = "ord_ref", insertable = false, updatable = false)
+    @JoinColumn(name = "fou_code", referencedColumnName = "fou_code", insertable = false, updatable = false)
+    private GeoOrdreLogistique logistique;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "fou_code", referencedColumnName = "fou_code")
-	private GeoFournisseur fournisseur;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fou_code", referencedColumnName = "fou_code")
+    private GeoFournisseur fournisseur;
 
-	@Column(name = "exp_nb_pal")
-	private Float nombrePalettesExpediees;
+    @Column(name = "exp_nb_pal")
+    private Float nombrePalettesExpediees;
 
-	@Column(name = "cde_nb_pal")
-	private Float nombrePalettesCommandees;
+    @Column(name = "cde_nb_pal")
+    private Float nombrePalettesCommandees;
 
-	@Column(name = "exp_nb_col")
-	private Float nombreColisExpedies;
+    @Column(name = "exp_nb_col")
+    private Float nombreColisExpedies;
 
-	@Column(name = "cde_nb_col")
-	private Float nombreColisCommandes;
+    @Column(name = "cde_nb_col")
+    private Float nombreColisCommandes;
 
-	@Column(name = "exp_pds_net")
-	private Float poidsNetExpedie;
+    @Column(name = "exp_pds_net")
+    private Float poidsNetExpedie;
 
-	@Column(name = "cde_pds_net")
-	private Float poidsNetCommande;
+    @Column(name = "cde_pds_net")
+    private Float poidsNetCommande;
 
-	@Column(name = "exp_pds_brut")
-	private Double poidsBrutExpedie;
+    @Column(name = "exp_pds_brut")
+    private Double poidsBrutExpedie;
 
-	@Column(name = "cde_pds_brut")
-	private Float poidsBrutCommande;
+    @Column(name = "cde_pds_brut")
+    private Float poidsBrutCommande;
 
-	@Column(name = "vte_pu")
-	private Float ventePrixUnitaire;
+    @Column(name = "vte_pu")
+    private Float ventePrixUnitaire;
 
-	@Column(name = "vte_qte")
-	private Double venteQuantite;
+    @Column(name = "vte_qte")
+    private Double venteQuantite;
 
-	@Column(name = "ach_pu")
-	private Double achatPrixUnitaire;
+    @Column(name = "ach_pu")
+    private Double achatPrixUnitaire;
 
-	@Column(name = "ach_dev_code")
-	private String achatDevise;
+    @Column(name = "ach_dev_code")
+    private String achatDevise;
 
-	@Column(name = "ach_dev_taux")
-	private Double achatDeviseTaux;
+    @Column(name = "ach_dev_taux")
+    private Double achatDeviseTaux;
 
-	@Column(name = "ach_dev_pu")
-	private Double achatDevisePrixUnitaire;
+    @Column(name = "ach_dev_pu")
+    private Double achatDevisePrixUnitaire;
 
-	@Column(name = "ach_qte")
-	private Double achatQuantite;
+    @Column(name = "ach_qte")
+    private Double achatQuantite;
 
-	@Column(name = "totvte")
-	private Double totalVenteBrut;
+    @Column(name = "totvte")
+    private Double totalVenteBrut;
 
-	@Column(name = "totrem")
-	private Float totalRemise;
+    @Column(name = "totrem")
+    private Float totalRemise;
 
-	@Column(name = "totres")
-	private Float totalRestitue;
+    @Column(name = "totres")
+    private Float totalRestitue;
 
-	@Column(name = "totfrd")
-	private Double totalFraisMarketing;
+    @Column(name = "totfrd")
+    private Double totalFraisMarketing;
 
-	@Column(name = "totach")
-	private Double totalAchat;
+    @Column(name = "totach")
+    private Double totalAchat;
 
-	@Column(name = "totmob")
-	private Float totalObjectifMarge;
+    @Column(name = "totmob")
+    private Float totalObjectifMarge;
 
-	@Column(name = "tottrp")
-	private Float totalTransport;
+    @Column(name = "tottrp")
+    private Float totalTransport;
 
-	@Column(name = "tottrs")
-	private Float totalTransit;
+    @Column(name = "tottrs")
+    private Float totalTransit;
 
-	@Column(name = "totcrt")
-	private Float totalCourtage;
+    @Column(name = "totcrt")
+    private Float totalCourtage;
 
-	@Column(name = "totfad")
-	private Float totalFraisAdditionnels;
+    @Column(name = "totfad")
+    private Float totalFraisAdditionnels;
 
-	@Column(name = "totfrais_plateforme")
-	private Float totalFraisPlateforme;
+    @Column(name = "totfrais_plateforme")
+    private Float totalFraisPlateforme;
 
-	@Column(name = "var_ristourne")
-	private Boolean ristourne;
+    @Column(name = "var_ristourne")
+    private Boolean ristourne;
 
-	@Column(name = "frais_pu")
-	private Double fraisPrixUnitaire;
+    @Column(name = "frais_pu")
+    private Double fraisPrixUnitaire;
 
-	@Column(name = "frais_desc")
-	private String fraisCommentaires;
+    @Column(name = "frais_desc")
+    private String fraisCommentaires;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "frais_unite")
-	private GeoBaseTarif fraisUnite;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "frais_unite")
+    private GeoBaseTarif fraisUnite;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "ach_bta_code")
-	private GeoBaseTarif achatUnite;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ach_bta_code")
+    private GeoBaseTarif achatUnite;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "vte_bta_code")
-	private GeoBaseTarif venteUnite;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "vte_bta_code")
+    private GeoBaseTarif venteUnite;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "bac_code")
-	private GeoBureauAchat bureauAchat;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bac_code")
+    private GeoBureauAchat bureauAchat;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumnOrFormula(formula = @JoinFormula("esp_code"))
-	@JoinColumnOrFormula(column = @JoinColumn(name = "tem_code"))
-	private GeoTemperature temperature;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumnOrFormula(formula = @JoinFormula("esp_code"))
+    @JoinColumnOrFormula(column = @JoinColumn(name = "tem_code"))
+    private GeoTemperature temperature;
 
-	@Column(name = "demipal_ind")
-	private Float indicateurPalette;
+    @Column(name = "demipal_ind")
+    private Float indicateurPalette;
 
-	@Column(name = "pal_nb_col")
-	private Float nombreColisPalette;
+    @Column(name = "pal_nb_col")
+    private Float nombreColisPalette;
 
-	@Column(name = "pal_nb_palinter")
-	private Float nombrePalettesIntermediaires;
+    @Column(name = "pal_nb_palinter")
+    private Float nombrePalettesIntermediaires;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "palinter_code")
-	private GeoTypePalette paletteInter;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "palinter_code")
+    private GeoTypePalette paletteInter;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "esp_code")
-	private GeoEspece espece;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "esp_code")
+    private GeoEspece espece;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "pal_code")
-	private GeoTypePalette typePalette;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pal_code")
+    private GeoTypePalette typePalette;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "grp_code")
-	private GeoGroupage lieuGroupage;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "grp_code")
+    private GeoGroupage lieuGroupage;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "trp_code")
-	private GeoTransporteur transporteurGroupage;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "trp_code")
+    private GeoTransporteur transporteurGroupage;
 
-	@Column(name = "ind_gratuit")
-	private Boolean gratuit;
+    @Column(name = "ind_gratuit")
+    private Boolean gratuit;
 
-	@Column(name = "orl_lig")
-	private String numero;
+    @Column(name = "orl_lig")
+    private String numero;
 
-	@Column(name = "pro_ref")
-	private String referenceProduit;
+    @Column(name = "pro_ref")
+    private String referenceProduit;
 
-	@Column(name = "pan_code")
-	private String palettisation;
+    @Column(name = "pan_code")
+    private String palettisation;
 
-	@Column(name = "lib_dlv")
-	private String libelleDLV;
+    @Column(name = "lib_dlv")
+    private String libelleDLV;
 
-	@Column(name = "obs_fourni")
-	private String observationsFournisseur;
+    @Column(name = "obs_fourni")
+    private String observationsFournisseur;
 
-	@Column(name = "cq_ref")
-	private String referenceControleQualite;
+    @Column(name = "cq_ref")
+    private String referenceControleQualite;
 
-	@Column(name = "stm_ref")
-	private String stockMouvement;
+    @Column(name = "stm_ref", insertable = false, updatable = false)
+    private String stockMouvement;
 
-	@Column(name = "flexp")
-	private Boolean expedie;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "ordreLigne")
+    private List<GeoStockMouvement> mouvement;
 
-	@Column(name = "flliv")
-	private Boolean livre;
+    @Column(name = "flexp")
+    private Boolean expedie;
 
-	@Column(name = "flbaf")
-	private Boolean bonAFacturer;
+    @Column(name = "flliv")
+    private Boolean livre;
 
-	@Column(name = "flfac")
-	private Boolean facture;
+    @Column(name = "flbaf")
+    private Boolean bonAFacturer;
 
-	@Column(name = "flverfou")
-	private Boolean verificationFournisseur;
+    @Column(name = "flfac")
+    private Boolean facture;
 
-	@Column(name = "indbloq_ach_dev_pu")
-	private Boolean indicateurBlocagePrix;
+    @Column(name = "flverfou")
+    private Boolean verificationFournisseur;
 
-	@Column(name = "remsf_tx")
-	private Float tauxRemiseSurFacture;
+    @Column(name = "indbloq_ach_dev_pu")
+    private Boolean indicateurBlocagePrix;
 
-	@Column(name = "remhf_tx")
-	private Float tauxRemiseHorsFacture;
+    @Column(name = "remsf_tx")
+    private Float tauxRemiseSurFacture;
 
-	@Column(name = "stock_nb_resa")
-	private Float nombreReservationsSurStock;
+    @Column(name = "remhf_tx")
+    private Float tauxRemiseHorsFacture;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "propr_code", referencedColumnName = "fou_code")
-	private GeoFournisseur proprietaireMarchandise;
+    @Column(name = "stock_nb_resa")
+    private Float nombreReservationsSurStock;
 
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "promo_code")
-	private GeoCodePromo codePromo;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "propr_code", referencedColumnName = "fou_code")
+    private GeoFournisseur proprietaireMarchandise;
 
-	@Column(name = "art_ref_kit")
-	private String articleKit;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "promo_code")
+    private GeoCodePromo codePromo;
 
-	@Column(name = "gtin_colis_kit")
-	private String gtinColisKit;
+    @Column(name = "art_ref_kit")
+    private String articleKit;
 
-	@NotNull
-	@Column(name = "nb_colis_manquant", nullable = false)
-	private Integer nombreColisManquant;
+    @Column(name = "gtin_colis_kit")
+    private String gtinColisKit;
 
-	@Column(name = "list_certifs")
-	private String listeCertifications;
+    @NotNull
+    @Column(name = "nb_colis_manquant", nullable = false)
+    private Integer nombreColisManquant;
 
-	@Column(name = "cert_origine")
-	private String origineCertification;
+    @Column(name = "list_certifs")
+    private String listeCertifications;
 
-	@Column(name = "pde_ref")
-	private String referenceProdet;
+    @Column(name = "cert_origine")
+    private String origineCertification;
 
-	@Column(name = "nb_cqphotos")
-	private Double nombrePhotosCQ;
+    @Column(name = "pde_ref")
+    private String referenceProdet;
 
-	@OneToMany(fetch = FetchType.LAZY, mappedBy = "ordreLigne")
-	private List<GeoTracabiliteLigne> tracabiliteLignes;
+    @Column(name = "nb_cqphotos")
+    private Double nombrePhotosCQ;
 
-	@Transient
-	private Float nombreColisPaletteByDimensions;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "ordreLigne")
+    private List<GeoTracabiliteLigne> tracabiliteLignes;
 
-	@Transient
-	private Double margeBrute;
+    @Transient
+    private Float nombreColisPaletteByDimensions;
 
-	@Transient
-	private Double pourcentageMargeBrute;
+    @Transient
+    private Double margeBrute;
 
-	@Transient
-	private Double pourcentageMargeNette;
+    @Transient
+    private Double pourcentageMargeBrute;
 
-	@PostLoad
-	public void postLoad() {
+    @Transient
+    private Double pourcentageMargeNette;
 
-		try {
-			this.margeBrute = (Double) (this.totalVenteBrut - this.totalRemise + this.totalRestitue
-					- this.totalFraisMarketing - this.totalAchat - this.totalTransport - this.totalTransit
-					- this.totalCourtage - this.totalFraisAdditionnels);
-			this.pourcentageMargeBrute = this.totalVenteBrut != 0d ? this.margeBrute / this.totalVenteBrut : 0d;
-			this.pourcentageMargeNette = this.totalVenteBrut != 0d
-					? (this.margeBrute - this.totalObjectifMarge) / this.totalVenteBrut
-					: 0d;
-		} catch (Exception e) {
-		}
+    @PostLoad
+    public void postLoad() {
 
-		GeoTypePalette typePalette = this.getTypePalette();
-		if (typePalette == null)
-			return;
-		Character dimensions = typePalette.getDimensions();
-		if (dimensions == null)
-			return;
+        try {
+            this.margeBrute = (Double) (this.totalVenteBrut - this.totalRemise + this.totalRestitue
+                    - this.totalFraisMarketing - this.totalAchat - this.totalTransport - this.totalTransit
+                    - this.totalCourtage - this.totalFraisAdditionnels);
+            this.pourcentageMargeBrute = this.totalVenteBrut != 0d ? this.margeBrute / this.totalVenteBrut : 0d;
+            this.pourcentageMargeNette = this.totalVenteBrut != 0d
+                    ? (this.margeBrute - this.totalObjectifMarge) / this.totalVenteBrut
+                    : 0d;
+        } catch (Exception e) {
+        }
 
-		this.nombreColisPaletteByDimensions = 0f;
+        GeoTypePalette typePalette = this.getTypePalette();
+        if (typePalette == null)
+            return;
+        Character dimensions = typePalette.getDimensions();
+        if (dimensions == null)
+            return;
 
-		if (this.getArticle() != null &&
-				this.getArticle().getEmballage() != null &&
-				this.getArticle().getEmballage().getEmballage() != null) {
+        this.nombreColisPaletteByDimensions = 0f;
 
-			if (dimensions == '1') {
-				Float xb = this.getArticle().getEmballage().getEmballage().getXb();
-				Float xh = this.getArticle().getEmballage().getEmballage().getXh();
-				if (xb != null && xh != null)
-					this.nombreColisPaletteByDimensions = xb * xh;
-			} else if (dimensions == '8') {
-				Float yb = this.getArticle().getEmballage().getEmballage().getYb();
-				Float yh = this.getArticle().getEmballage().getEmballage().getYh();
-				if (yb != null && yh != null)
-					this.nombreColisPaletteByDimensions = yb * yh;
-			} else if (dimensions == '6') {
-				Float zb = this.getArticle().getEmballage().getEmballage().getZb();
-				Float zh = this.getArticle().getEmballage().getEmballage().getZh();
-				if (zb != null && zh != null)
-					this.nombreColisPaletteByDimensions = zb * zh;
-			}
-		}
-	}
+        if (this.getArticle() != null &&
+                this.getArticle().getEmballage() != null &&
+                this.getArticle().getEmballage().getEmballage() != null) {
+
+            if (dimensions == '1') {
+                Float xb = this.getArticle().getEmballage().getEmballage().getXb();
+                Float xh = this.getArticle().getEmballage().getEmballage().getXh();
+                if (xb != null && xh != null)
+                    this.nombreColisPaletteByDimensions = xb * xh;
+            } else if (dimensions == '8') {
+                Float yb = this.getArticle().getEmballage().getEmballage().getYb();
+                Float yh = this.getArticle().getEmballage().getEmballage().getYh();
+                if (yb != null && yh != null)
+                    this.nombreColisPaletteByDimensions = yb * yh;
+            } else if (dimensions == '6') {
+                Float zb = this.getArticle().getEmballage().getEmballage().getZb();
+                Float zh = this.getArticle().getEmballage().getEmballage().getZh();
+                if (zb != null && zh != null)
+                    this.nombreColisPaletteByDimensions = zb * zh;
+            }
+        }
+    }
 
 }
