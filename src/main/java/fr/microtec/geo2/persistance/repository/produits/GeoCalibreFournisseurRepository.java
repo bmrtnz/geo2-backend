4a6c7aaaa138bf71c0749586337fc21bdbde32a9
Thomas <thomas@microtec.fr>
2022-07-18 16:39:27 +0200

    fix `CalibreFournisseur.getOne` GQL endpoint

--- src/main/java/fr/microtec/geo2/persistance/repository/produits/GeoCalibreFournisseurRepository.java
+++ src/main/java/fr/microtec/geo2/persistance/repository/produits/GeoCalibreFournisseurRepository.java
@@ -1,9 +1,13 @@
 package fr.microtec.geo2.persistance.repository.produits;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
 import fr.microtec.geo2.persistance.entity.produits.GeoCalibreFournisseur;
 import fr.microtec.geo2.persistance.repository.GeoRepository;
-import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GeoCalibreFournisseurRepository extends GeoRepository<GeoCalibreFournisseur, String> {
+    Optional<GeoCalibreFournisseur> findOneByIdAndEspeceId(String id, String especeId);
 }
