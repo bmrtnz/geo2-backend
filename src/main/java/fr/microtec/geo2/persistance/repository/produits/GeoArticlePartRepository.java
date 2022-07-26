65e12ff51ca1fd311eb4dde722d716daa1355492
valentin <valentin@microtec.fr>
2022-07-26 16:18:55 +0200

    fix last commit

--- src/main/java/fr/microtec/geo2/persistance/repository/produits/GeoArticlePartRepository.java
+++ src/main/java/fr/microtec/geo2/persistance/repository/produits/GeoArticlePartRepository.java
@@ -2,6 +2,7 @@ package fr.microtec.geo2.persistance.repository.produits;
 
 import fr.microtec.geo2.persistance.repository.GeoRepository;
 import fr.microtec.geo2.persistance.repository.produits.matcher.GeoArticlePartMatchable;
+import org.springframework.data.repository.NoRepositoryBean;
 
 import java.io.Serializable;
 
@@ -11,6 +12,7 @@ import java.io.Serializable;
  * but not avi_art_gestion (This is master table).
  *
  */
+@NoRepositoryBean
 public interface GeoArticlePartRepository<T, ID extends Serializable>
     extends GeoRepository<T, ID>, GeoArticlePartMatchable<T> {
 }
