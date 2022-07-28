4bc064dad822cdccd4fe175feeabb11754fe6815
Thomas <thomas@microtec.fr>
2022-07-28 11:51:46 +0200

    update `allLigneReservation` query/model #17180

--- src/main/java/fr/microtec/geo2/persistance/entity/stock/GeoLigneReservation.java
+++ src/main/java/fr/microtec/geo2/persistance/entity/stock/GeoLigneReservation.java
@@ -4,22 +4,62 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import fr.microtec.geo2.persistance.entity.ValidateAndModifiedEntity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 @Entity
-public class GeoLigneReservation {
-    @Id
-    @Column(name = "rownum")
-    private Integer id;
+public class GeoLigneReservation extends ValidateAndModifiedEntity {
 
-    @Column(name = "fou_code")
+    @Id
+    @Column(name = "stm_ref")
+    private String id;
+
+    @Column(name = "sto_ref")
+    private String stockId;
+
+    @Column(name = "nom_utilisateur")
+    private String nomUtilisateur;
+
+    @Column(name = "mvt_type")
+    private Character type;
+
+    @Column(name = "mvt_qte")
+    private Integer quantite;
+
+    @Column(name = "ord_ref")
+    private String ordreId;
+
+    @Column(name = "art_ref")
+    private String articleId;
+
+    @Column(name = "orl_ref")
+    private String ordreLigneId;
+
+    @Column(name = "stm_desc")
+    private String mouvementDescription;
+
+    @Column(name = "ligne_fournisseur_code")
+    private String ligneFournisseurCode;
+
+    @Column(name = "stock_fournisseur_code")
     private String fournisseurCode;
 
     @Column(name = "prop_code")
     private String proprietaireCode;
 
-    @Column(name = "mvt_qte")
-    private Integer quantite;
+    @Column(name = "sto_desc")
+    private String stockDescription;
+
+    @Column(name = "qte_ini")
+    private Integer quantiteInitiale;
+
+    @Column(name = "qte_res")
+    private Integer quantiteReservee;
+
+    @Column(name = "pal_code")
+    private String typePalette;
 
 }
