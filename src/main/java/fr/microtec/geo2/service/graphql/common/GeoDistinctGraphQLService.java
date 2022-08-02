b7183c3507a93c1e275ebad3a6cb4c675c3c0a69
Thomas <thomas@microtec.fr>
2022-08-02 17:41:50 +0200

    add support for enums on `Distinct` query #17358

--- src/main/java/fr/microtec/geo2/service/graphql/common/GeoDistinctGraphQLService.java
+++ src/main/java/fr/microtec/geo2/service/graphql/common/GeoDistinctGraphQLService.java
@@ -1,9 +1,14 @@
 package fr.microtec.geo2.service.graphql.common;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Expression;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,6 +24,7 @@ import fr.microtec.geo2.configuration.graphql.PageFactory;
 import fr.microtec.geo2.configuration.graphql.RelayPage;
 import fr.microtec.geo2.persistance.CriteriaUtils;
 import fr.microtec.geo2.persistance.EntityUtils;
+import fr.microtec.geo2.persistance.StringEnum;
 import fr.microtec.geo2.persistance.entity.Distinct;
 import fr.microtec.geo2.persistance.rsql.GeoCustomVisitor;
 import io.leangen.graphql.annotations.GraphQLArgument;
@@ -31,73 +37,106 @@ import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 @Secured("ROLE_USER")
 public class GeoDistinctGraphQLService {
 
-	private final EntityManager entityManager;
-	private final RSQLParser rsqlParser;
+    private final EntityManager entityManager;
+    private final RSQLParser rsqlParser;
 
-	public GeoDistinctGraphQLService(EntityManager entityManager, RSQLParser rsqlParser) {
-		this.entityManager = entityManager;
-		this.rsqlParser = rsqlParser;
-	}
+    public GeoDistinctGraphQLService(EntityManager entityManager, RSQLParser rsqlParser) {
+        this.entityManager = entityManager;
+        this.rsqlParser = rsqlParser;
+    }
 
-	@GraphQLQuery
-	public RelayPage<Distinct> getDistinct(
-			@GraphQLArgument(name = "type") String inputType,
-			@GraphQLArgument(name = "field") String requestField,
-			@GraphQLArgument(name = "search") String search,
-			@GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable) {
-		Specification<?> spec = null;
-		if (search != null && !search.isBlank()) {
-			spec = this.rsqlParser.parse(search).accept(new GeoCustomVisitor<>());
-		}
+    @GraphQLQuery
+    public RelayPage<Distinct> getDistinct(
+            @GraphQLArgument(name = "type") String inputType,
+            @GraphQLArgument(name = "field") String requestField,
+            @GraphQLArgument(name = "search") String search,
+            @GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable) {
+        Specification<?> spec = null;
+        if (search != null && !search.isBlank()) {
+            spec = this.rsqlParser.parse(search).accept(new GeoCustomVisitor<>());
+        }
 
-		Page<Distinct> result = this.readPage(spec, pageable, EntityUtils.getEntityClassFromName(inputType),
-				requestField);
+        Page<Distinct> result = this.readPage(spec, pageable, EntityUtils.getEntityClassFromName(inputType),
+                requestField);
 
-		return PageFactory.asRelayPage(result);
-	}
+        return PageFactory.asRelayPage(result);
+    }
 
-	/**
-	 * Read page with criteria query.
-	 * Query select distinct {requestedField} from associated table on {inputType}
-	 * entity.
-	 * Apply specification if present and paging parameters.
-	 *
-	 * @param spec           Specification to apply.
-	 * @param pageable       Pageable parameter.
-	 * @param entityClass    Entity class.
-	 * @param requestedField Entity requested field.
-	 * @return Request page data.
-	 */
-	private Page<Distinct> readPage(
-			Specification<?> spec, Pageable pageable,
-			Class<?> entityClass, String requestedField) {
-		CriteriaBuilder criteriaBuilder = this.entityManager.getCriteriaBuilder();
-		CriteriaQuery<Distinct> criteriaQuery = CriteriaUtils.selectCountDistinct(
-				criteriaBuilder, entityClass, requestedField, spec);
+    /**
+     * Read page with criteria query.
+     * Query select distinct {requestedField} from associated table on {inputType}
+     * entity.
+     * Apply specification if present and paging parameters.
+     *
+     * @param spec           Specification to apply.
+     * @param pageable       Pageable parameter.
+     * @param entityClass    Entity class.
+     * @param requestedField Entity requested field.
+     * @return Request page data.
+     */
+    private Page<Distinct> readPage(
+            Specification<?> spec, Pageable pageable,
+            Class<?> entityClass, String requestedField) {
 
-		// Order
-		Sort sort = pageable.isPaged() ? pageable.getSort() : Sort.unsorted();
-		if (sort.isSorted()) {
-			criteriaQuery.orderBy(
-					CriteriaUtils.toOrders(sort, CriteriaUtils.findRoot(criteriaQuery, entityClass), criteriaBuilder));
-		}
+        CriteriaBuilder criteriaBuilder = this.entityManager.getCriteriaBuilder();
+        CriteriaQuery<Distinct> criteriaQuery = CriteriaUtils.selectCountDistinct(
+                criteriaBuilder, entityClass, requestedField, spec);
 
-		TypedQuery<Distinct> query = this.entityManager.createQuery(criteriaQuery);
+        // Handle enum types
+        Expression<?> distinctExpr = CriteriaUtils
+                .toExpressionRecursively(criteriaQuery.from(entityClass), requestedField, true);
+        Class<?> type = distinctExpr.getJavaType();
+        if (StringEnum.class.isAssignableFrom(type)) {
+            return readEnumPage(type, pageable);
+        }
 
-		// Paging
-		if (pageable.isPaged()) {
-			query.setFirstResult((int) pageable.getOffset());
-			query.setMaxResults(pageable.getPageSize());
-		}
+        // Order
+        Sort sort = pageable.isPaged() ? pageable.getSort() : Sort.unsorted();
+        if (sort.isSorted()) {
+            criteriaQuery.orderBy(
+                    CriteriaUtils.toOrders(sort, CriteriaUtils.findRoot(criteriaQuery, entityClass), criteriaBuilder));
+        }
 
-		CriteriaQuery<Long> countCriteriaQuery = CriteriaUtils.countDistinct(criteriaBuilder, entityClass,
-				requestedField, spec);
-		TypedQuery<Long> countQuery = this.entityManager.createQuery(countCriteriaQuery);
+        TypedQuery<Distinct> query = this.entityManager.createQuery(criteriaQuery);
 
-		return PageableExecutionUtils.getPage(
-				query.getResultList(),
-				pageable,
-				() -> CustomUtils.executeCountQuery(countQuery));
-	}
+        // Paging
+        if (pageable.isPaged()) {
+            query.setFirstResult((int) pageable.getOffset());
+            query.setMaxResults(pageable.getPageSize());
+        }
+
+        CriteriaQuery<Long> countCriteriaQuery = CriteriaUtils.countDistinct(criteriaBuilder, entityClass,
+                requestedField, spec);
+        TypedQuery<Long> countQuery = this.entityManager.createQuery(countCriteriaQuery);
+
+        return PageableExecutionUtils.getPage(
+                query.getResultList(),
+                pageable,
+                () -> CustomUtils.executeCountQuery(countQuery));
+    }
+
+    public Page<Distinct> readEnumPage(Class<?> type, Pageable pageable) {
+        Class<?>[] interfaces = type.getInterfaces();
+        Page<Distinct> page = Page.empty();
+
+        for (Class<?> inter : interfaces) {
+            if (StringEnum.class.equals(inter)) {
+                try {
+                    List<String> keys = (List<String>) inter
+                            .getMethod("getKeys", Class.class)
+                            .invoke(null, type);
+                    List<Distinct> result = keys.stream()
+                            .map(k -> new Distinct(k, -1l))
+                            .collect(Collectors.toList());
+                    page = PageableExecutionUtils.getPage(result, pageable, () -> -1);
+                } catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException
+                        | NoSuchMethodException | SecurityException e) {
+                    throw new RuntimeException(String.format("Unable to load Page of distinct '%s'", type));
+                }
+            }
+        }
+
+        return page;
+    }
 
 }
