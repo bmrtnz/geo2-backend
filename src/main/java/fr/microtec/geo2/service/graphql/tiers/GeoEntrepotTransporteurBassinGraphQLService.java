f2f22bfc4a17b8b2e99e70dd402fd8af17c45f5c
Thomas <thomas@microtec.fr>
2022-08-09 09:55:55 +0200

    add `GeoEntrepotTransporteurBassin` GQL endpoint #17551

---
+++ src/main/java/fr/microtec/geo2/service/graphql/tiers/GeoEntrepotTransporteurBassinGraphQLService.java
@@ -0,0 +1,53 @@
+package fr.microtec.geo2.service.graphql.tiers;
+
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Service;
+
+import fr.microtec.geo2.configuration.graphql.RelayPage;
+import fr.microtec.geo2.persistance.entity.tiers.GeoEntrepotTransporteurBassin;
+import fr.microtec.geo2.persistance.repository.tiers.GeoEntrepotTransporteurBassinRepository;
+import fr.microtec.geo2.service.graphql.GeoAbstractGraphQLService;
+import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLEnvironment;
+import io.leangen.graphql.annotations.GraphQLMutation;
+import io.leangen.graphql.annotations.GraphQLNonNull;
+import io.leangen.graphql.annotations.GraphQLQuery;
+import io.leangen.graphql.execution.ResolutionEnvironment;
+import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
+
+@Service
+@GraphQLApi
+@Secured("ROLE_USER")
+public class GeoEntrepotTransporteurBassinGraphQLService
+        extends GeoAbstractGraphQLService<GeoEntrepotTransporteurBassin, BigDecimal> {
+
+    public GeoEntrepotTransporteurBassinGraphQLService(GeoEntrepotTransporteurBassinRepository repository) {
+        super(repository, GeoEntrepotTransporteurBassin.class);
+    }
+
+    @GraphQLQuery
+    public RelayPage<GeoEntrepotTransporteurBassin> allEntrepotTransporteurBassin(
+            @GraphQLArgument(name = "search") String search,
+            @GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
+            @GraphQLEnvironment ResolutionEnvironment env) {
+        return this.getPage(search, pageable, env);
+    }
+
+    @GraphQLQuery
+    public Optional<GeoEntrepotTransporteurBassin> getEntrepotTransporteurBassin(
+            @GraphQLArgument(name = "id") BigDecimal id) {
+        return this.getOne(id);
+    }
+
+    @GraphQLMutation
+    public GeoEntrepotTransporteurBassin saveEntrepotTransporteurBassin(
+            GeoEntrepotTransporteurBassin EntrepotTransporteurBassin,
+            @GraphQLEnvironment ResolutionEnvironment env) {
+        return this.saveEntity(EntrepotTransporteurBassin, env);
+    }
+
+}
