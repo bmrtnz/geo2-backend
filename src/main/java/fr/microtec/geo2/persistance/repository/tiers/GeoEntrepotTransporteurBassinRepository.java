3454657fb80d60526177baf96083ccf99f3cd46a
Thomas <thomas@microtec.fr>
2022-08-29 12:21:26 +0200

    add `entrepot-transporteur-bassin` change logique #17551

--- src/main/java/fr/microtec/geo2/persistance/repository/tiers/GeoEntrepotTransporteurBassinRepository.java
+++ src/main/java/fr/microtec/geo2/persistance/repository/tiers/GeoEntrepotTransporteurBassinRepository.java
@@ -4,10 +4,13 @@ import java.math.BigDecimal;
 
 import org.springframework.stereotype.Repository;
 
+import fr.microtec.geo2.persistance.entity.tiers.GeoBureauAchat;
+import fr.microtec.geo2.persistance.entity.tiers.GeoEntrepot;
 import fr.microtec.geo2.persistance.entity.tiers.GeoEntrepotTransporteurBassin;
 import fr.microtec.geo2.persistance.repository.GeoRepository;
 
 @Repository
 public interface GeoEntrepotTransporteurBassinRepository
         extends GeoRepository<GeoEntrepotTransporteurBassin, BigDecimal> {
+    BigDecimal deleteByEntrepotAndBureauAchat(GeoEntrepot entrepot, GeoBureauAchat bureauAchat);
 }
