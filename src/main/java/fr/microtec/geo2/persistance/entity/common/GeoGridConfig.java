4b1c4ebf61954f28c32e7b77ac0f31c76ee55a5f
Thomas <thomas@microtec.fr>
2022-09-15 15:07:43 +0200

    update `GeoGridConfig` entity structure #16112

--- src/main/java/fr/microtec/geo2/persistance/entity/common/GeoGridConfig.java
+++ src/main/java/fr/microtec/geo2/persistance/entity/common/GeoGridConfig.java
@@ -1,33 +1,44 @@
 package fr.microtec.geo2.persistance.entity.common;
 
+import java.util.HashMap;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import fr.microtec.geo2.persistance.entity.ModifiedEntity;
+import fr.microtec.geo2.persistance.entity.tiers.GeoSociete;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.ToString;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
-import org.hibernate.annotations.LazyGroup;
-
-import javax.persistence.*;
-import java.util.HashMap;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Table(name = "geo_grid_config")
 @IdClass(GeoGridConfigKey.class)
 @Entity
-public class GeoGridConfig {
+public class GeoGridConfig extends ModifiedEntity {
 
-	@Id
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "utilisateur")
-	private GeoUtilisateur utilisateur;
+    @Id
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "utilisateur")
+    private GeoUtilisateur utilisateur;
 
-	@Id
-	@Column(name = "grid")
-	private String grid;
+    @Id
+    @Column(name = "grid")
+    private String grid;
 
-	@Lob
-	@Column(name = "config", columnDefinition = "BLOB")
-	private HashMap<String, Object> config;
+    @Lob
+    @Column(name = "config", columnDefinition = "BLOB")
+    private HashMap<String, Object> config;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "soc_code")
+    private GeoSociete societe;
 
 }
