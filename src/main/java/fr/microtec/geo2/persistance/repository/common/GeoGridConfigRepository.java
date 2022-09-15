4b7b48ddfc42693da7af960bb620fa76a1fe5080
Thomas <thomas@microtec.fr>
2022-09-15 17:15:48 +0200

    update `GeoGridConfig` repo/endpoints #16112

--- src/main/java/fr/microtec/geo2/persistance/repository/common/GeoGridConfigRepository.java
+++ src/main/java/fr/microtec/geo2/persistance/repository/common/GeoGridConfigRepository.java
@@ -1,10 +1,17 @@
 package fr.microtec.geo2.persistance.repository.common;
 
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
 import fr.microtec.geo2.persistance.entity.common.GeoGridConfig;
 import fr.microtec.geo2.persistance.entity.common.GeoGridConfigKey;
 import fr.microtec.geo2.persistance.repository.GeoRepository;
-import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GeoGridConfigRepository extends GeoRepository<GeoGridConfig, GeoGridConfigKey> {
+    Optional<GeoGridConfig> getOneByGridAndUtilisateurNomUtilisateurAndSocieteId(
+            String grid,
+            String nomUtilisateur,
+            String societe);
 }
--- src/main/java/fr/microtec/geo2/service/graphql/common/GeoGridConfigGraphQLService.java
+++ src/main/java/fr/microtec/geo2/service/graphql/common/GeoGridConfigGraphQLService.java
@@ -24,33 +24,40 @@ import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 @Secured("ROLE_USER")
 public class GeoGridConfigGraphQLService extends GeoAbstractGraphQLService<GeoGridConfig, GeoGridConfigKey> {
 
-	public GeoGridConfigGraphQLService(GeoGridConfigRepository repository) {
-		super(repository, GeoGridConfig.class);
-	}
+    public GeoGridConfigGraphQLService(GeoGridConfigRepository repository) {
+        super(repository, GeoGridConfig.class);
+    }
 
-	@GraphQLQuery
-	public RelayPage<GeoGridConfig> allGridConfig(
-			@GraphQLArgument(name = "search") String search,
-			@GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
-			@GraphQLEnvironment ResolutionEnvironment env
-	) {
-		return this.getPage(search, pageable, env);
-	}
+    @GraphQLQuery
+    public RelayPage<GeoGridConfig> allGridConfig(
+            @GraphQLArgument(name = "search") String search,
+            @GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
+            @GraphQLEnvironment ResolutionEnvironment env) {
+        return this.getPage(search, pageable, env);
+    }
 
-	@GraphQLQuery
-	public Optional<GeoGridConfig> getGridConfig(
-			@GraphQLArgument(name = "id") GeoGridConfigKey id
-	) {
-		return super.getOne(id);
-  }
+    @GraphQLQuery
+    public Optional<GeoGridConfig> getGridConfig(
+            @GraphQLArgument(name = "id") GeoGridConfigKey id) {
+        return super.getOne(id);
+    }
 
-  	@GraphQLMutation
-	public GeoGridConfig saveGridConfig(GeoGridConfig gridConfig, @GraphQLEnvironment ResolutionEnvironment env) {
-		return this.saveEntity(gridConfig, env);
-	}
+    @GraphQLQuery
+    public Optional<GeoGridConfig> getGridConfigBySociete(
+            @GraphQLArgument(name = "grid") String grid,
+            @GraphQLArgument(name = "utilisateur") String nomUtilisateur,
+            @GraphQLArgument(name = "societe") String societe) {
+        return ((GeoGridConfigRepository) super.repository)
+                .getOneByGridAndUtilisateurNomUtilisateurAndSocieteId(grid, nomUtilisateur, societe);
+    }
 
-	@GraphQLMutation
-	public void deleteGridConfig(GeoGridConfigKey id) {
-		this.delete(id);
-	}
+    @GraphQLMutation
+    public GeoGridConfig saveGridConfig(GeoGridConfig gridConfig, @GraphQLEnvironment ResolutionEnvironment env) {
+        return this.saveEntity(gridConfig, env);
+    }
+
+    @GraphQLMutation
+    public void deleteGridConfig(GeoGridConfigKey id) {
+        this.delete(id);
+    }
 }
