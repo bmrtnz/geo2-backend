8308fdb83b6fd128dcddbe1d2147debe42fc059d
Thomas <thomas@microtec.fr>
2022-10-24 12:12:10 +0200

    add `GeoDocumentNum` entity #17045

---
+++ src/main/java/fr/microtec/geo2/persistance/entity/ordres/GeoDocumentNum.java
@@ -0,0 +1,62 @@
+package fr.microtec.geo2.persistance.entity.ordres;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "geo_docnum")
+@Data
+@IdClass(GeoDocumentNumKey.class)
+public class GeoDocumentNum {
+
+    @Id
+    @Column(name = "ord_num")
+    protected String ordreNumero;
+
+    @Id
+    @Column(name = "typ_doc")
+    protected String typeDocument;
+
+    @Id
+    @Column(name = "cre_an")
+    protected String anneeCreation;
+
+    @Column(name = "cre_mois")
+    protected String moisCreation;
+
+    @Column(name = "nom_partage")
+    protected String nomPartage;
+
+    @Column(name = "id")
+    protected String id;
+
+    @Column(name = "nordre")
+    protected String numeroOrdre;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "orl_ref")
+    protected GeoOrdreLigne ordreLigne;
+
+    @Column(name = "nom_fic")
+    protected String nomFic;
+
+    @Column(name = "nom_fic_complet")
+    protected String nomFicComplet;
+
+    @Column(name = "commentaire")
+    protected String commentaire;
+
+    @Column(name = "flpdf")
+    protected Boolean flagPDF;
+
+    @Column(name = "status")
+    protected Integer statut;
+}
