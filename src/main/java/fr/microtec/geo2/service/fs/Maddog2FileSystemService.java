d41dfdf3a8bae9743c989cf01d758533937ead7d
Thomas <thomas@microtec.fr>
2022-10-25 14:58:47 +0200

    add `cqTechnique` document field #17045

--- src/main/java/fr/microtec/geo2/service/fs/Maddog2FileSystemService.java
+++ src/main/java/fr/microtec/geo2/service/fs/Maddog2FileSystemService.java
@@ -1,14 +1,14 @@
 package fr.microtec.geo2.service.fs;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.access.annotation.Secured;
-import org.springframework.stereotype.Service;
-
 import java.nio.file.FileSystemNotFoundException;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.stereotype.Service;
 
 /**
  * This service is used for access to maddog2 NAS file system.
@@ -17,87 +17,92 @@ import java.util.List;
 @Secured("ROLE_USER")
 public class Maddog2FileSystemService extends FileSystemService {
 
-	/**
-	 * PATH_KEY use by client, it's use to order files.
-	 */
-	public enum PATH_KEY {
-		CLIENTS("/Geo2_doc/tiers/clients/"),
-		FOURNISSEURS("/Geo2_doc/tiers/fournisseurs/"),
-		TRANSPORTEURS("/Geo2_doc/tiers/transporteurs/"),
-		LIEUX_PASSAGE_A_QUAI("/Geo2_doc/tiers/lieuxpassageaquai/"),
-		ARTICLES("/Geo2_doc/articles/"),
-		ORDRES("/Geo2_doc/ordres/"),
-		GEO_IMG("/geo_img/"),
-		GEO_DOC("/geo_doc/"),
+    /**
+     * PATH_KEY use by client, it's use to order files.
+     */
+    public enum PATH_KEY {
+        CLIENTS("/Geo2_doc/tiers/clients/"),
+        FOURNISSEURS("/Geo2_doc/tiers/fournisseurs/"),
+        TRANSPORTEURS("/Geo2_doc/tiers/transporteurs/"),
+        LIEUX_PASSAGE_A_QUAI("/Geo2_doc/tiers/lieuxpassageaquai/"),
+        ARTICLES("/Geo2_doc/articles/"),
+        ORDRES("/Geo2_doc/ordres/"),
+        GEO_IMG("/geo_img/"),
+        GEO_DOC("/geo_doc/"),
         GEO_FACTURE("/geo_factures/"),
-        GEO_RETOUR_PALOX("/geo_retour_palox/");
+        GEO_RETOUR_PALOX("/geo_retour_palox/"),
+        GEO_CQ("/geo_cq/");
 
-		private String path;
-        //private String urlPath;
-		PATH_KEY(String basePath/*, String urlPath*/) { this.path = basePath; /*this.urlPath = urlPath;*/ }
+        private String path;
 
-		public static PATH_KEY from(String s) {
-			for (PATH_KEY pathKey : PATH_KEY.values()) {
-				if (pathKey.name().equalsIgnoreCase(s)) return pathKey;
-			}
+        // private String urlPath;
+        PATH_KEY(String basePath/* , String urlPath */) {
+            this.path = basePath;
+            /* this.urlPath = urlPath; */ }
 
-			throw new IllegalArgumentException();
-		}
-	}
+        public static PATH_KEY from(String s) {
+            for (PATH_KEY pathKey : PATH_KEY.values()) {
+                if (pathKey.name().equalsIgnoreCase(s))
+                    return pathKey;
+            }
 
-	public Maddog2FileSystemService(@Value("${geo2.maddog2.path}") String basePath) {
-		super();
-		this.basePath = basePath;
-	}
+            throw new IllegalArgumentException();
+        }
+    }
 
-	/**
-	 * Build base relative path for given path_key and id.
-	 */
-	public String buildKeyIdPath(String key, String id) {
-		return PATH_KEY.from(key).path + id + "/";
-	}
+    public Maddog2FileSystemService(@Value("${geo2.maddog2.path}") String basePath) {
+        super();
+        this.basePath = basePath;
+    }
 
-	/**
-	 * Get path of document from given path in pathKey.
-	 * Download args :
-	 *  true : get full path for access to file
-	 *  false: get relative path for download via FileManagerController
-	 */
-	public Path getDocument(PATH_KEY pathKey, String path, boolean download) {
-		Path file = this._getBasePath(pathKey.path).resolve(path);
+    /**
+     * Build base relative path for given path_key and id.
+     */
+    public String buildKeyIdPath(String key, String id) {
+        return PATH_KEY.from(key).path + id + "/";
+    }
 
-		if (!Files.exists(file)) {
-			throw new FileSystemNotFoundException();
-		}
+    /**
+     * Get path of document from given path in pathKey.
+     * Download args :
+     * true : get full path for access to file
+     * false: get relative path for download via FileManagerController
+     */
+    public Path getDocument(PATH_KEY pathKey, String path, boolean download) {
+        Path file = this._getBasePath(pathKey.path).resolve(path);
 
-		return download ? file : Path.of(path);
-	}
+        if (!Files.exists(file)) {
+            throw new FileSystemNotFoundException();
+        }
 
-	/**
-	 * Get Path of label file from given name, with extension or not.
-	 */
-	public Path getEtiquette(String filename) {
-		boolean withExtension = filename.matches(HAVE_EXTENSION_REGEX);
-		String globPattern = "glob:**/" + filename + (withExtension ? "" : ".{pdf,jpg}");
-		List<Path> files = this.list(PATH_KEY.GEO_IMG.path, FileSystems.getDefault().getPathMatcher(globPattern));
-		Path downloadFile;
+        return download ? file : Path.of(path);
+    }
 
-		// If as multiple file, pdf is priority
-		if (files.size() > 1) {
-			boolean asPdf = files.stream().anyMatch(f -> f.toString().endsWith(".pdf"));
+    /**
+     * Get Path of label file from given name, with extension or not.
+     */
+    public Path getEtiquette(String filename) {
+        boolean withExtension = filename.matches(HAVE_EXTENSION_REGEX);
+        String globPattern = "glob:**/" + filename + (withExtension ? "" : ".{pdf,jpg}");
+        List<Path> files = this.list(PATH_KEY.GEO_IMG.path, FileSystems.getDefault().getPathMatcher(globPattern));
+        Path downloadFile;
 
-			if (asPdf) {
-				downloadFile = files.stream().filter(f -> f.toString().endsWith(".pdf")).findFirst().get();
-			} else {
-				downloadFile = files.get(0);
-			}
-		} else if (files.size() == 1) {
-			downloadFile = files.get(0);
-		} else {
-			throw new FileSystemNotFoundException();
-		}
+        // If as multiple file, pdf is priority
+        if (files.size() > 1) {
+            boolean asPdf = files.stream().anyMatch(f -> f.toString().endsWith(".pdf"));
 
-		return downloadFile;
-	}
+            if (asPdf) {
+                downloadFile = files.stream().filter(f -> f.toString().endsWith(".pdf")).findFirst().get();
+            } else {
+                downloadFile = files.get(0);
+            }
+        } else if (files.size() == 1) {
+            downloadFile = files.get(0);
+        } else {
+            throw new FileSystemNotFoundException();
+        }
+
+        return downloadFile;
+    }
 
 }
--- src/main/java/fr/microtec/geo2/service/graphql/ordres/GeoCQLigneGraphQLService.java
+++ src/main/java/fr/microtec/geo2/service/graphql/ordres/GeoCQLigneGraphQLService.java
@@ -2,8 +2,6 @@ package fr.microtec.geo2.service.graphql.ordres;
 
 import java.util.Optional;
 
-import io.leangen.graphql.annotations.GraphQLEnvironment;
-import io.leangen.graphql.execution.ResolutionEnvironment;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
@@ -11,10 +9,13 @@ import org.springframework.stereotype.Service;
 import fr.microtec.geo2.configuration.graphql.RelayPage;
 import fr.microtec.geo2.persistance.entity.ordres.GeoCQLigne;
 import fr.microtec.geo2.persistance.repository.ordres.GeoCQLigneRepository;
+import fr.microtec.geo2.service.DocumentService;
 import fr.microtec.geo2.service.graphql.GeoAbstractGraphQLService;
 import io.leangen.graphql.annotations.GraphQLArgument;
+import io.leangen.graphql.annotations.GraphQLEnvironment;
 import io.leangen.graphql.annotations.GraphQLNonNull;
 import io.leangen.graphql.annotations.GraphQLQuery;
+import io.leangen.graphql.execution.ResolutionEnvironment;
 import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 
 @Service
@@ -22,24 +23,28 @@ import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 @Secured("ROLE_USER")
 public class GeoCQLigneGraphQLService extends GeoAbstractGraphQLService<GeoCQLigne, String> {
 
-	public GeoCQLigneGraphQLService(GeoCQLigneRepository repository) {
-		super(repository, GeoCQLigne.class);
-	}
+    private final DocumentService documentService;
 
-	@GraphQLQuery
-	public RelayPage<GeoCQLigne> allCQLigne(
-			@GraphQLArgument(name = "search") String search,
-			@GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
-			@GraphQLEnvironment ResolutionEnvironment env
-	) {
-		return this.getPage(search, pageable, env);
-	}
+    public GeoCQLigneGraphQLService(GeoCQLigneRepository repository, DocumentService documentService) {
+        super(repository, GeoCQLigne.class);
+        this.documentService = documentService;
+    }
 
-	@GraphQLQuery
-	public Optional<GeoCQLigne> getCQLigne(
-			@GraphQLArgument(name = "id") String id
-	) {
-		return super.getOne(id);
-  }
+    @GraphQLQuery
+    public RelayPage<GeoCQLigne> allCQLigne(
+            @GraphQLArgument(name = "search") String search,
+            @GraphQLArgument(name = "pageable") @GraphQLNonNull Pageable pageable,
+            @GraphQLEnvironment ResolutionEnvironment env) {
+
+        return this.documentService
+                .loadDocuments(this.getPage(search, pageable, env));
+    }
+
+    @GraphQLQuery
+    public Optional<GeoCQLigne> getCQLigne(
+            @GraphQLArgument(name = "id") String id) {
+        return this.documentService
+                .loadDocuments(super.getOne(id));
+    }
 
 }
