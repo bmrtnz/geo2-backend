77763575ede26aa5afb2925ad0f8bc3101c36572
Thomas <thomas@microtec.fr>
2022-11-03 09:41:25 +0100

    update `allClientEnCours` query/entity

--- src/main/java/fr/microtec/geo2/persistance/entity/tiers/GeoClientEnCours.java
+++ src/main/java/fr/microtec/geo2/persistance/entity/tiers/GeoClientEnCours.java
@@ -23,8 +23,9 @@ public class GeoClientEnCours {
     @JoinColumn(name = "soc_code")
     private GeoSociete societe;
 
-    @Column(name = "cen_ref")
-    String refEntrepot;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cen_ref")
+    private GeoEntrepot entrepot;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "cli_ref")
