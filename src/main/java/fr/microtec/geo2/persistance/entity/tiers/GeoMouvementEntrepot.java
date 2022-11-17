4518919baaf87f6c70b9a695cc3f212c02e1e9c4
Thomas <thomas@microtec.fr>
2022-11-17 09:42:58 +0100

    fix `mouvement-entrepot` query #18495

--- src/main/java/fr/microtec/geo2/persistance/entity/tiers/GeoMouvementEntrepot.java
+++ src/main/java/fr/microtec/geo2/persistance/entity/tiers/GeoMouvementEntrepot.java
@@ -1,5 +1,7 @@
 package fr.microtec.geo2.persistance.entity.tiers;
 
+import java.time.LocalDateTime;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 
@@ -11,8 +13,29 @@ import lombok.EqualsAndHashCode;
 @Entity
 public class GeoMouvementEntrepot extends GeoSupervisionPalox {
 
+    @Column(name = "nordre")
+    String numeroOrdre;
+
+    @Column(name = "depdatp")
+    LocalDateTime dateDepartOrdre;
+
+    @Column(name = "bon_retour")
+    String bonRetour;
+
+    @Column(name = "cmr")
+    String cmr;
+
+    @Column(name = "ref_cli")
+    String referenceClient;
+
     @Column(name = "cli_ref")
     String codeClient;
+
+    @Column(name = "palox_ko_nbr")
+    Double nombrePaloxKO;
+
+    @Column(name = "palox_ko_cause")
+    Double nombrePaloxCause;
 
     @Column(name = "station")
     String station;
