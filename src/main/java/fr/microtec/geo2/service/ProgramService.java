b58af0bb05e0a2e9ec37878836f49bcfe02c0e5c
Thomas <thomas@microtec.fr>
2022-11-22 16:10:50 +0100

    fix program service #18635

--- src/main/java/fr/microtec/geo2/service/ProgramService.java
+++ src/main/java/fr/microtec/geo2/service/ProgramService.java
@@ -2,6 +2,9 @@ package fr.microtec.geo2.service;
 
 import java.io.InputStream;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProgramService {
 
     public Object importTesco(InputStream stream) {
