92a3fede637d16ce746eb4f95d1829bb99cf98a2
Thomas <thomas@microtec.fr>
2022-12-06 20:10:42 +0100

    register `GeoLigneChargement` query to repository

--- src/main/java/fr/microtec/geo2/persistance/repository/ordres/GeoOrdreRepository.java
+++ src/main/java/fr/microtec/geo2/persistance/repository/ordres/GeoOrdreRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import fr.microtec.geo2.persistance.entity.common.GeoCampagne;
+import fr.microtec.geo2.persistance.entity.ordres.GeoLigneChargement;
 import fr.microtec.geo2.persistance.entity.ordres.GeoOrdre;
 import fr.microtec.geo2.persistance.entity.ordres.GeoPlanningTransporteur;
 import fr.microtec.geo2.persistance.entity.tiers.GeoSociete;
@@ -27,4 +28,9 @@ public interface GeoOrdreRepository extends GeoRepository<GeoOrdre, String> {
             @Param("arg_date_max") LocalDateTime dateMax,
             @Param("arg_soc_code") String societeCode,
             @Param("arg_trp_code") String transporteurCode);
+
+    @Query(name = "Ordre.allLignesChargement", nativeQuery = true)
+    List<GeoLigneChargement> allLignesChargement(
+            @Param("arg_code_chargement") String codeChargement,
+            @Param("arg_cam_code") String campagne);
 }
