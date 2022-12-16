6b7efc9ad941fe0adb1216f671473bd5775f776a
Thomas <thomas@microtec.fr>
2022-12-16 12:09:04 +0100

    update `F_RETURN_FORFAITS_TRP` SQL procedure

--- sql/f_return_forfaits_trp.sql
+++ sql/f_return_forfaits_trp.sql
@@ -52,6 +52,7 @@ CREATE OR REPLACE PROCEDURE "GEO_ADMIN".F_RETURN_FORFAITS_TRP(
     arg_trp_dev_pu number,
     arg_bta_code varchar2,
     arg_dev_code varchar2,
+    arg_typ_ordre varchar2,
     res OUT number,
     msg OUT varchar2,
     li_ret OUT number
@@ -60,6 +61,7 @@ AS
     ls_soc_code varchar2(50);
     ls_sco_code varchar2(50);
     ls_gcl_code varchar2(50);
+    ls_ind_forftrp varchar2(50);
 BEGIN
     res := 0;
     msg := '';
@@ -73,6 +75,29 @@ BEGIN
     If  ls_gcl_code is null Then
         ls_gcl_code := '';
     ENd If;
+
+    If arg_typ_ordre is not null and arg_typ_ordre <> ''  then
+
+        select IND_FORFTRP into ls_ind_forftrp
+        FROM GEO_TYPORD
+        where TYP_ORD =arg_typ_ordre ;
+
+        if ls_ind_forftrp = 'N'  then
+            res := 0;
+            return;
+        End If;
+
+    End If;
+
+    If ls_soc_code <>'SA'  Then
+        res := 0;
+        return;
+    end if;
+
+    If ls_sco_code <> 'F'  Then
+        res := 0;
+        return;
+    end if;
 
     select count(*) into li_ret
     from GEO_FORFAITS_TRP
@@ -93,17 +118,13 @@ BEGIN
 
     ENd IF;
 
-
-
-
-
-    -- If li_ret >0 Then
-        -- select TRP_UNITE,DEV_CODE,TRP_PU into arg_bta_code, arg_dev_code,arg_trp_dev_pu
-        -- from GEO_FORFAITS_TRP
-        -- where	GCL_CODE =ls_gcl_code and
-        --             INC_CODE =arg_inc_code and
-        --             VALIDE ='O';
-    -- End If;
+    If li_ret >0 Then
+        select TRP_UNITE,DEV_CODE,TRP_PU into arg_bta_code, arg_dev_code,arg_trp_dev_pu
+        from GEO_FORFAITS_TRP
+        where	GCL_CODE =ls_gcl_code and
+                    INC_CODE =arg_inc_code and
+                    VALIDE ='O';
+    End If;
 
     res := 1;
 
