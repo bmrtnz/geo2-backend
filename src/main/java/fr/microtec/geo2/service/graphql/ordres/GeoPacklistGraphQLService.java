a5dbfab609db5f133dc7aa2e125c51a7ba9e1af0
Thomas <thomas@microtec.fr>
2022-12-19 11:15:48 +0100

    fix `Packlist@save`

--- src/main/java/fr/microtec/geo2/service/graphql/ordres/GeoPacklistGraphQLService.java
+++ src/main/java/fr/microtec/geo2/service/graphql/ordres/GeoPacklistGraphQLService.java
@@ -2,10 +2,14 @@ package fr.microtec.geo2.service.graphql.ordres;
 
 import java.math.BigDecimal;
 
+import javax.persistence.EntityManager;
+
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import fr.microtec.geo2.persistance.entity.ordres.GeoPacklistEntete;
+import fr.microtec.geo2.persistance.entity.ordres.GeoPacklistOrdre;
 import fr.microtec.geo2.persistance.repository.ordres.GeoPacklistRepository;
 import fr.microtec.geo2.service.graphql.GeoAbstractGraphQLService;
 import io.leangen.graphql.annotations.GraphQLEnvironment;
@@ -18,13 +22,27 @@ import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 @Secured("ROLE_USER")
 public class GeoPacklistGraphQLService extends GeoAbstractGraphQLService<GeoPacklistEntete, BigDecimal> {
 
-    public GeoPacklistGraphQLService(GeoPacklistRepository repository) {
+    private final EntityManager entityManager;
+
+    public GeoPacklistGraphQLService(GeoPacklistRepository repository, EntityManager entityManager) {
         super(repository, GeoPacklistEntete.class);
+        this.entityManager = entityManager;
     }
 
     @GraphQLMutation
-    public GeoPacklistEntete savePacklist(GeoPacklistEntete packlist, @GraphQLEnvironment ResolutionEnvironment env) {
-        return this.saveEntity(packlist, env);
+    @Transactional
+    public GeoPacklistEntete savePacklistEntete(GeoPacklistEntete packlistEntete,
+            @GraphQLEnvironment ResolutionEnvironment env) {
+        GeoPacklistEntete e = this.saveEntity(packlistEntete, env);
+
+        e.getOrdres().parallelStream().forEach(o -> {
+            GeoPacklistOrdre m = new GeoPacklistOrdre();
+            m.setId(e.getId());
+            m.setOrdre(o.getOrdre());
+            this.entityManager.persist(m);
+        });
+
+        return this.repository.getOne(e.getId());
     }
 
 }
