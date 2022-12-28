d93fc55e9e214ff90679ed275efc3f6ceac01a2d
Thomas <thomas@microtec.fr>
2022-12-28 04:04:27 +0100

    add `capitalize` function to `StringUtils`

--- src/main/java/fr/microtec/geo2/common/StringUtils.java
+++ src/main/java/fr/microtec/geo2/common/StringUtils.java
@@ -2,18 +2,25 @@ package fr.microtec.geo2.common;
 
 public class StringUtils {
 
-	/**
-	 * Pad string left with given delimiter for given size.
-	 */
-	public static String padLeft(String s, String delimiter, int size) {
-		return String.format("%1$" + size + "s", s).replace(" ", delimiter);
-	}
+    /**
+     * Pad string left with given delimiter for given size.
+     */
+    public static String padLeft(String s, String delimiter, int size) {
+        return String.format("%1$" + size + "s", s).replace(" ", delimiter);
+    }
 
-	/**
-	 * Pad string right with given delimiter for given size.
-	 */
-	public static String padRight(String s, String delimiter, int size) {
-		return String.format("%1$-" + size + "s", s).replace(" ", delimiter);
-	}
+    /**
+     * Pad string right with given delimiter for given size.
+     */
+    public static String padRight(String s, String delimiter, int size) {
+        return String.format("%1$-" + size + "s", s).replace(" ", delimiter);
+    }
+
+    /**
+     * Change the case of the first character to uppercase
+     */
+    public static String capitalize(String s) {
+        return s.substring(0, 1).toUpperCase() + s.substring(1).toLowerCase();
+    }
 
 }
