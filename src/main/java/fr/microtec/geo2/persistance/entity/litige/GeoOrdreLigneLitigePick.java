f08448903184a1ce587d8fd499b5b7cc51b5083e
Thomas <thomas@microtec.fr>
2023-02-15 17:57:44 +0100

    add `GeoOrdreLigneLitigePick` entity

---
+++ src/main/java/fr/microtec/geo2/persistance/entity/litige/GeoOrdreLigneLitigePick.java
@@ -0,0 +1,28 @@
+package fr.microtec.geo2.persistance.entity.litige;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import fr.microtec.geo2.persistance.entity.ordres.GeoOrdreLigne;
+import lombok.Data;
+
+@Data
+@Entity
+public class GeoOrdreLigneLitigePick {
+
+    @Id
+    @Column(name = "rownum")
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "orl_ref")
+    private GeoOrdreLigne ordreLigne;
+
+    @Column(name = "tot_nb_col")
+    private Integer totalNombreColis;
+
+}
