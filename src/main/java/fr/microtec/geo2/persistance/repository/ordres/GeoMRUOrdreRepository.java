4bf59e642a3edb3917e2a4975f657b6189fbf006
Thomas <thomas@microtec.fr>
2023-06-06 00:34:31 +0200

    update `historique MRU ordre` query

--- src/main/java/fr/microtec/geo2/persistance/repository/ordres/GeoMRUOrdreRepository.java
+++ src/main/java/fr/microtec/geo2/persistance/repository/ordres/GeoMRUOrdreRepository.java
@@ -12,6 +12,6 @@ import fr.microtec.geo2.persistance.repository.GeoRepository;
 
 @Repository
 public interface GeoMRUOrdreRepository extends GeoRepository<GeoMRUOrdre, GeoMRUOrdreKey> {
-    @Query("SELECT mru FROM #{#entityName} mru INNER JOIN GeoOrdre o on o.id = mru.ordreRef WHERE mru.societe.id = :societe and mru.ordreRef is not null and mru.utilisateur.nomUtilisateur = :utilisateur and mru.dateModification >= :dateMax and rownum <= :count")
+    @Query(value = "SELECT * FROM ( SELECT mru.* FROM GEO_MRU_ORDRE mru INNER JOIN geo_ordre o ON o.ORD_REF = mru.ord_ref WHERE mru.SOC_CODE = :societe AND mru.ORD_REF IS NOT NULL AND mru.NOM_UTILISATEUR = :utilisateur AND mru.MOD_DATE >= :dateMax ORDER BY mru.MOD_DATE DESC ) WHERE rownum <= :count", nativeQuery = true)
     List<GeoMRUOrdre> findHead(String societe, String utilisateur, LocalDateTime dateMax, Long count);
 }
