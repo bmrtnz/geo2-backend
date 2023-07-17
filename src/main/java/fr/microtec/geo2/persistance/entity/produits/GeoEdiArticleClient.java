fd9cec3eef39b1332b0e92a4df9379a648ae78d6
Thomas <thomas@microtec.fr>
2023-07-17 10:35:50 +0200

    register generator on `GeoEdiArticleClient` entity

--- src/main/java/fr/microtec/geo2/persistance/entity/produits/GeoEdiArticleClient.java
+++ src/main/java/fr/microtec/geo2/persistance/entity/produits/GeoEdiArticleClient.java
@@ -3,11 +3,15 @@ package fr.microtec.geo2.persistance.entity.produits;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
 
 import fr.microtec.geo2.persistance.entity.ValidateAndModifiedEntity;
 import fr.microtec.geo2.persistance.entity.tiers.GeoClient;
@@ -20,6 +24,11 @@ public class GeoEdiArticleClient extends ValidateAndModifiedEntity {
 
     @Id
     @Column(name = "k_edi_art_cli")
+    @GeneratedValue(generator = "GeoEdiArticleClientGenerator")
+    @GenericGenerator(name = "GeoEdiArticleClientGenerator", strategy = "fr.microtec.geo2.persistance.GeoSequenceGenerator", parameters = {
+            @Parameter(name = "sequenceName", value = "F_SEQ_EDI_ART_CLI"),
+            @Parameter(name = "isSequence", value = "false")
+    })
     private Integer id;
 
     @NotNull
