e1b09ecf71832d875ebc8e811137258ab18c3961
Thomas <thomas@microtec.fr>
2023-09-19 09:35:39 +0200

    add `bureauAchat` to `PlanningTransporteurs`

--- src/main/java/fr/microtec/geo2/persistance/entity/ordres/GeoPlanningTransporteur.java
+++ src/main/java/fr/microtec/geo2/persistance/entity/ordres/GeoPlanningTransporteur.java
@@ -6,11 +6,14 @@ import java.time.LocalDateTime;
 import javax.persistence.Column;
 import javax.persistence.Convert;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
 
 import fr.microtec.geo2.persistance.converter.BooleanCharacterConverter;
+import fr.microtec.geo2.persistance.entity.tiers.GeoBureauAchat;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -19,85 +22,90 @@ import lombok.Setter;
 @Entity
 public class GeoPlanningTransporteur {
 
-  @Id
-  @Column(name = "rownum")
-  Integer id;
+    @Id
+    @Column(name = "rownum")
+    Integer id;
 
-  @Column(name = "code_chargement")
-  String codeChargement;
+    @Column(name = "code_chargement")
+    String codeChargement;
 
-  @Column(name = "nordre")
-  String numero;
+    @Column(name = "nordre")
+    String numero;
 
-  @Column(name = "livdatp")
-  LocalDate dateLivraisonPrevue;
+    @Column(name = "livdatp")
+    LocalDate dateLivraisonPrevue;
 
-  @Column(name = "ref_cli")
-  String referenceClient;
+    @Column(name = "ref_cli")
+    String referenceClient;
 
-  @Column(name = "version_ordre")
-  String version;
+    @Column(name = "version_ordre")
+    String version;
 
-  @Column(name = "somme_colis_commandes")
-  Double sommeColisCommandes;
+    @Column(name = "somme_colis_commandes")
+    Double sommeColisCommandes;
 
-  @Column(name = "somme_colis_palette")
-  Double sommeColisPalette;
+    @Column(name = "somme_colis_palette")
+    Double sommeColisPalette;
 
-  @Column(name = "somme_colis_palette_bis")
-  Double sommeColisPaletteBis;
+    @Column(name = "somme_colis_palette_bis")
+    Double sommeColisPaletteBis;
 
-  @Column(name = "cen_code")
-  String entrepot;
+    @Column(name = "cen_code")
+    String entrepot;
 
-  @Column(name = "raisoc")
-  String entrepotRaisonSocial;
+    @Column(name = "raisoc")
+    String entrepotRaisonSocial;
 
-  @Column(name = "ezip")
-  String entrepotCodePostal;
+    @Column(name = "ezip")
+    String entrepotCodePostal;
 
-  @Column(name = "ville")
-  String entrepotVille;
+    @Column(name = "ville")
+    String entrepotVille;
 
-  @Column(name = "epay")
-  String entrepotPays;
+    @Column(name = "epay")
+    String entrepotPays;
 
-  @Column(name = "datdep_fou_p")
-  LocalDateTime dateDepartPrevueFournisseur;
+    @Column(name = "datdep_fou_p")
+    LocalDateTime dateDepartPrevueFournisseur;
 
-  @Column(name = "fou_code")
-  String fournisseur;
+    @Column(name = "fou_code")
+    String fournisseur;
 
-  @Column(name = "zip")
-  String fournisseurCodePostal;
+    @Column(name = "zip")
+    String fournisseurCodePostal;
 
-  @Column(name = "pay_code")
-  String fournisseurPays;
+    @Column(name = "pay_code")
+    String fournisseurPays;
 
-  @Column(name = "datdep_grp_p")
-  LocalDateTime dateDepartPrevueGroupage;
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "bac_code", nullable = false)
+    private GeoBureauAchat fournisseurBassin;
 
-  @Column(name = "grp_code")
-  String groupage;
+    @Column(name = "datdep_grp_p")
+    LocalDateTime dateDepartPrevueGroupage;
 
-  @Column(name = "esp_code")
-  String espece;
+    @Column(name = "grp_code")
+    String groupage;
 
-  @Column(name = "pal_code")
-  String palette;
+    @Column(name = "esp_code")
+    String espece;
 
-  @Column(name = "trp_code")
-  String transporteur;
+    @Column(name = "pal_code")
+    String palette;
 
-  @Column(name = "flag_entrepot")
-  @Convert(converter = BooleanCharacterConverter.class)
-  Boolean flagEntrepot;
+    @Column(name = "trp_code")
+    String transporteur;
 
-  @Column(name = "col_code")
-  String colis;
+    @Column(name = "flag_entrepot")
+    @Convert(converter = BooleanCharacterConverter.class)
+    Boolean flagEntrepot;
 
-  @ManyToOne
-  @JoinColumn(name = "ord_ref", insertable = false, updatable = false)
-  private GeoOrdre ordre;
+    @Column(name = "col_code")
+    String colis;
+
+    @ManyToOne
+    @JoinColumn(name = "ord_ref", insertable = false, updatable = false)
+    private GeoOrdre ordre;
 
 }
