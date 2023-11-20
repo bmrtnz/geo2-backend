92c699a3ca6ae35c0df65b93ed0c4a779e9c4f07
Thomas <thomas@microtec.fr>
2023-11-20 14:48:31 +0100

    update `GeoAlerte` entity

--- src/main/java/fr/microtec/geo2/persistance/entity/common/GeoAlerte.java
+++ src/main/java/fr/microtec/geo2/persistance/entity/common/GeoAlerte.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -12,9 +13,12 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 import org.hibernate.annotations.GenericGenerator;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import fr.microtec.geo2.persistance.entity.ValidateEntity;
 import fr.microtec.geo2.persistance.entity.tiers.GeoSecteur;
@@ -27,6 +31,9 @@ import lombok.Setter;
 @EqualsAndHashCode(callSuper = true)
 @Entity
 @Table(name = "geo_alert")
+@DynamicInsert
+@DynamicUpdate
+@EntityListeners(AuditingEntityListener.class)
 public class GeoAlerte extends ValidateEntity {
 
     @Id
