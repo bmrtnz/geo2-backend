b6eec3017346ac8340fc875233dedcc4d960e07c
Thomas <thomas@microtec.fr>
2023-11-22 09:25:09 +0100

    update `fetchAlert` query

--- src/main/java/fr/microtec/geo2/persistance/repository/common/GeoAlerteRepository.java
+++ src/main/java/fr/microtec/geo2/persistance/repository/common/GeoAlerteRepository.java
@@ -3,6 +3,7 @@ package fr.microtec.geo2.persistance.repository.common;
 import java.math.BigDecimal;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import fr.microtec.geo2.persistance.entity.common.GeoAlerte;
@@ -10,5 +11,6 @@ import fr.microtec.geo2.persistance.repository.GeoRepository;
 
 @Repository
 public interface GeoAlerteRepository extends GeoRepository<GeoAlerte, BigDecimal> {
-    Optional<GeoAlerte> findTopByOrderByDateCreationDesc();
+    @Query(value = "SELECT * FROM GEO_ALERT WHERE k_alert = GET_ID_ALERT('F')", nativeQuery = true)
+    Optional<GeoAlerte> findTopAlert();
 }
